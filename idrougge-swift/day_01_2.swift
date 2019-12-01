be10f024c29b8110d1b84275ba6beb0c67a0ec2d
Iggy Drougge <idrougge@mac.com>
2019-12-01 23:31:17 +0100

    Dag 1 i Swift

---
+++ idrougge-swift/day_01_2.swift
@@ -0,0 +1,26 @@
+#!/usr/bin/env xcrun swift
+
+import Foundation
+
+guard CommandLine.arguments.count == 2, CommandLine.arguments[1].isEmpty == false
+    else { 
+        print("Specify file name")
+        exit(0)
+}
+
+func fuel(for mass: Int) -> Int? {
+    let mass = mass / 3 - 2
+    guard mass > 0 else { return nil }
+    return mass
+}
+
+let mass = try! String(contentsOfFile: CommandLine.arguments[1])
+    .components(separatedBy: .newlines)
+    .compactMap(Int.init)
+    .compactMap(fuel)
+    .lazy
+    .map({sequence(first: $0, next: fuel)})
+    .map({$0.reduce(0, +)})
+    .reduce(0, +)
+
+print(mass)
