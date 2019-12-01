3b9ee40c16634d9c835962b76e23d47299b66fdc
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2019-12-01 11:22:19 +0100

    fixed some indentation

--- estomagordo-python3/day_1a.py
+++ estomagordo-python3/day_1a.py
@@ -6,12 +6,12 @@ from collections import Counter, defaultdict
 
 def solve(d):
     return sum(x // 3 - 2 for x in d)
-	
+
 
 def read_and_solve():
-	with open('input_1.txt') as f:
-		data = [int(line.rstrip()) for line in f]
-		return solve(data)
+    with open('input_1.txt') as f:
+        data = [int(line.rstrip()) for line in f]
+        return solve(data)
 
 if __name__ == '__main__':
-	print(read_and_solve())
+    print(read_and_solve())
--- estomagordo-python3/day_1b.py
+++ estomagordo-python3/day_1b.py
@@ -5,20 +5,20 @@ from collections import Counter, defaultdict
 
 
 def solve(d):
-	fuel = 0
+    fuel = 0
 
-	for x in d:
-		while x // 3 - 2 > 0:
-			x = x // 3 - 2
-			fuel += x
+    for x in d:
+        while x // 3 - 2 > 0:
+            x = x // 3 - 2
+            fuel += x
 
-	return fuel
-	
+    return fuel
+    
 
 def read_and_solve():
-	with open('input_1.txt') as f:
-		data = [int(line.rstrip()) for line in f]
-		return solve(data)
+    with open('input_1.txt') as f:
+        data = [int(line.rstrip()) for line in f]
+        return solve(data)
 
 if __name__ == '__main__':
-	print(read_and_solve())
+    print(read_and_solve())
