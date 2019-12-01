261799c958264e6b116b82b41323cd5cc0485ad7
Tomas Skare <tomas.skare@pfsw.com>
2019-12-01 21:40:25 +0100

    tomasskare-rust: Day 1

---
+++ tomasskare-rust/day1/src/main.rs
@@ -0,0 +1,37 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+fn fuelneed(mass: i32) -> i32 {
+    let fuel = mass / 3 - 2;
+
+    if fuel <= 0 {
+        return 0;
+    }
+
+    return fuel + fuelneed(fuel);
+}
+
+fn main() {
+    let filename = "input.txt";
+
+    let fd = File::open(filename).expect(&format!("Failure opening {}", filename));
+    let buf = BufReader::new(fd);
+    let mut sum = 0;
+    buf.lines().for_each(|line| {
+        let num = line.unwrap().parse::<i32>().unwrap();
+        sum += num / 3 - 2;
+    });
+
+    println!("Part 1: {}", sum);
+
+    let fd = File::open(filename).expect(&format!("Failure opening {}", filename));
+    let buf = BufReader::new(fd);
+    let mut sum = 0;
+    buf.lines().for_each(|line| {
+        let num = line.unwrap().parse::<i32>().unwrap();
+        sum += fuelneed(num);
+    });
+
+    println!("Part 2: {}", sum);
+}
