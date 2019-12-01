eab3ac4c5fd15f2c49ba600fdf00ece565218ecb
Erik Dyrelius <erik.dyrelius@gmail.com>
2019-12-01 06:47:07 +0100

    Update to boilerplate

--- Dyr-El-python/dayXX.py
+++ Dyr-El-python/dayXX.py
@@ -24,13 +24,14 @@ def part2(pinp):
 
 if __name__ == "__main__":
     inp = readInput()
-
+    # inp = """"""
     
+    ## Update for input specifics ##############################################
     parseInp = fileParse(inp)
 
     print("Input is '" + str(parseInp[:10]) + 
           ('...' if len(parseInp)>10 else '') + "'")
-    print("Solution to part 1: " + part1(parseInp))
-    print("Solution to part 2: " + part2(parseInp))
+    print("Solution to part 1: {}".format(part1(parseInp)))
+    print("Solution to part 2: {}".format(part2(parseInp)))
 
 ## End of footer boilerplate ###################################################
