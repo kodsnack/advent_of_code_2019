73b331be4e48984f0991d3e90a05198377531ce2
Erik Dyrelius <erik.dyrelius@gmail.com>
2019-12-02 06:46:22 +0100

    The ugly solution

--- Dyr-El-python/day02.py
+++ Dyr-El-python/day02.py
@@ -9,22 +9,54 @@ def lineParse(s, f, fp):
         raise s
     return tuple(map(f, m.groups()))
 
-def fileParse(inp, f=lineParse, ff=lambda x:x, fp=re.compile(r"^(.*)$")):
-    return tuple(map(lambda x:f(x, ff, fp), inp.splitlines()))
+def fileParse(inp):
+    return tuple(map(int, inp.split(',')))
 
 ## End of header boilerplate ###################################################
 
+def run(pinp, n, v):
+    mem = dict()
+    a = 0
+    for i in pinp:
+        mem[a] = i
+        a += 1
+    a = 0
+    mem[1] = n
+    mem[2] = v
+    while mem.get(a, 0)!=99:
+        if mem.get(a,0) == 1:
+            #print("Add", a, mem.get(a,0), mem.get(a+1,0), mem.get(a+2,0), mem.get(a+3,0))
+            mem[mem.get(a+3,0)] = (mem.get(mem.get(a+1,0), 0) 
+                                 + mem.get(mem.get(a+2,0), 0))
+            a += 4
+        elif mem.get(a, 0) == 2:
+            #print("Mul", a, mem.get(a,0), mem.get(a+1,0), mem.get(a+2,0), mem.get(a+3,0))
+            mem[mem.get(a+3,0)] = (mem.get(mem.get(a+1,0), 0) 
+                                 * mem.get(mem.get(a+2,0), 0))
+            a += 4
+        else:
+            print("Error", a, mem[a])
+            raise "Error"
+
+    return mem[0]
+
 def part1(pinp):
-    return "<solution1>"
+    return run(pinp, 12, 2)
 
 def part2(pinp):
+    for n in range(100):
+        for v in range(100):
+            print(n, v)
+            if run(pinp, n, v)==19690720:
+                return 100*n+v
     return "<solution2>"
 
 ## Start of footer boilerplate #################################################
 
 if __name__ == "__main__":
     inp = readInput()
-    # inp = """"""
+    
+    #inp = """1,9,10,3,2,3,11,0,99,30,40,50"""
 
     ## Update for input specifics ##############################################    
     parseInp = fileParse(inp)
