3e69a1cd2c97f63a932a83a57bf770dab8c2b021
Erik Dyrelius <erik.dyrelius@scania.com>
2019-12-02 08:37:13 +0100

    Cleaner solution to python

--- Dyr-El-python/day02.py
+++ Dyr-El-python/day02.py
@@ -9,60 +9,68 @@ def lineParse(s, f, fp):
         raise s
     return tuple(map(f, m.groups()))
 
-def fileParse(inp):
-    return tuple(map(int, inp.split(',')))
+def fileParse(inp, f=lineParse, ff=lambda x:x, fp=re.compile(r"^(.*)$")):
+    return tuple(map(lambda x:f(x, ff, fp), inp.splitlines()))
 
 ## End of header boilerplate ###################################################
 
-def run(pinp, n, v):
-    mem = dict()
-    a = 0
-    for i in pinp:
-        mem[a] = i
-        a += 1
-    a = 0
-    mem[1] = n
-    mem[2] = v
-    while mem.get(a, 0)!=99:
-        if mem.get(a,0) == 1:
-            #print("Add", a, mem.get(a,0), mem.get(a+1,0), mem.get(a+2,0), mem.get(a+3,0))
-            mem[mem.get(a+3,0)] = (mem.get(mem.get(a+1,0), 0) 
-                                 + mem.get(mem.get(a+2,0), 0))
-            a += 4
-        elif mem.get(a, 0) == 2:
-            #print("Mul", a, mem.get(a,0), mem.get(a+1,0), mem.get(a+2,0), mem.get(a+3,0))
-            mem[mem.get(a+3,0)] = (mem.get(mem.get(a+1,0), 0) 
-                                 * mem.get(mem.get(a+2,0), 0))
-            a += 4
-        else:
-            print("Error", a, mem[a])
-            raise "Error"
+def isAdd(mem, address):
+    return mem[address] == 1
 
+def add(mem, address):
+    target = mem[address+3]
+    arg1 = mem[address+1]
+    arg2 = mem[address+2]
+    mem[target] = mem[arg1] + mem[arg2]
+
+def isMul(mem, address):
+    return mem[address] == 2
+
+def mul(mem, address):
+    target = mem[address+3]
+    arg1 = mem[address+1]
+    arg2 = mem[address+2]
+    mem[target] = mem[arg1] * mem[arg2]
+
+def isHalt(mem, address):
+    return mem[address] == 99
+
+def run(pinp, noun, verb):
+    mem = {address: value for address, value in enumerate(pinp[0][0])}
+    mem[1] = noun
+    mem[2] = verb
+    address = 0
+    while not isHalt(mem, address):
+        if isAdd(mem, address):
+            add(mem, address)
+            address += 4
+        elif isMul(mem, address):
+            mul(mem, address)
+            address += 4
+        else:
+            raise RuntimeError("Invalid instruction {} at address {}".format(mem[address], address))
     return mem[0]
 
 def part1(pinp):
     return run(pinp, 12, 2)
 
 def part2(pinp):
-    for n in range(100):
-        for v in range(100):
-            print(n, v)
-            if run(pinp, n, v)==19690720:
-                return 100*n+v
-    return "<solution2>"
+    for noun in range(100):
+        for verb in range(100):
+            if run(pinp, noun, verb)==19690720:
+                return 100 * noun + verb
+    return "Not found"
 
 ## Start of footer boilerplate #################################################
 
 if __name__ == "__main__":
     inp = readInput()
     
-    #inp = """1,9,10,3,2,3,11,0,99,30,40,50"""
-
     ## Update for input specifics ##############################################    
-    parseInp = fileParse(inp)
+    parseInp = fileParse(inp, ff=lambda x:tuple(map(int, x.split(','))))
 
-    print("Input is '" + str(parseInp[:10]) + 
-          ('...' if len(parseInp)>10 else '') + "'")
+    print("Input is '" + str(parseInp[:10])[:100] + 
+          ('...' if len(parseInp)>10 or len(str(parseInp[:10]))>100 else '') + "'")
     print("Solution to part 1: {}".format(part1(parseInp)))
     print("Solution to part 2: {}".format(part2(parseInp)))
 
