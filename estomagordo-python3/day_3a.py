151078521bcd11a12baa9db98de21e98caedd84d
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2019-12-03 10:26:33 +0100

    tests for 3 and much nicer implementations

--- estomagordo-python3/day_3a.py
+++ estomagordo-python3/day_3a.py
@@ -4,50 +4,38 @@ from heapq import heappop, heappush
 from collections import Counter, defaultdict
 
 
+def wire_to_places(wire):
+    places = {}
+    steps = 0
+
+    pos = [0, 0]
+
+    for instruction in wire:
+        direction = instruction[0]
+        dist = int(instruction[1:])
+
+        delta = [1, 0]
+
+        if direction == 'R':
+            delta = [0, 1]
+        if direction == 'L':
+            delta = [0, -1]
+        if direction == 'D':
+            delta = [-1, 0]
+
+        for _ in range(dist):
+            steps += 1
+            pos = [pos[0] + delta[0], pos[1] + delta[1]]
+            places[tuple(pos)] = steps
+
+    return places
+
+
 def solve(wires):
-    aplaces = set()
-    closest = 10**10
-
-    apos = [0, 0]
-
-    for instruction in wires[0].split(','):
-        direction = instruction[0]
-        dist = int(instruction[1:])
-
-        delta = [1, 0]
-
-        if direction == 'R':
-            delta = [0, 1]
-        if direction == 'L':
-            delta = [0, -1]
-        if direction == 'D':
-            delta = [-1, 0]
-
-        for x in range(dist):
-            apos = [apos[0] + delta[0], apos[1] + delta[1]]
-            aplaces.add(tuple(apos))
-
-    bpos = [0, 0]
-
-    for instruction in wires[1].split(','):
-        direction = instruction[0]
-        dist = int(instruction[1:])
-
-        delta = [1, 0]
-
-        if direction == 'R':
-            delta = [0, 1]
-        if direction == 'L':
-            delta = [0, -1]
-        if direction == 'D':
-            delta = [-1, 0]
-
-        for x in range(dist):
-            bpos = [bpos[0] + delta[0], bpos[1] + delta[1]]
-            if tuple(bpos) in aplaces:
-                closest = min(closest, abs(bpos[0]) + abs(bpos[1]))
-
-    return closest
+    aplaces = wire_to_places(wires[0].split(','))
+    bplaces = wire_to_places(wires[1].split(','))
+    
+    return min(abs(k[0]) + abs(k[1]) for k in aplaces.keys() & bplaces.keys())
 
 def read_and_solve():
     with open('input_3.txt') as f:
--- estomagordo-python3/day_3b.py
+++ estomagordo-python3/day_3b.py
@@ -4,58 +4,38 @@ from heapq import heappop, heappush
 from collections import Counter, defaultdict
 
 
+def wire_to_places(wire):
+    places = {}
+    steps = 0
+
+    pos = [0, 0]
+
+    for instruction in wire:
+        direction = instruction[0]
+        dist = int(instruction[1:])
+
+        delta = [1, 0]
+
+        if direction == 'R':
+            delta = [0, 1]
+        if direction == 'L':
+            delta = [0, -1]
+        if direction == 'D':
+            delta = [-1, 0]
+
+        for _ in range(dist):
+            steps += 1
+            pos = [pos[0] + delta[0], pos[1] + delta[1]]
+            places[tuple(pos)] = steps
+
+    return places
+
+
 def solve(wires):
-    intersections = {}
-    aplaces = {}
-    closest = 10**10
-
-    apos = [0, 0]
-    asteps = 0
-
-    for instruction in wires[0].split(','):
-        direction = instruction[0]
-        dist = int(instruction[1:])
-
-        delta = [1, 0]
-
-        if direction == 'R':
-            delta = [0, 1]
-        if direction == 'L':
-            delta = [0, -1]
-        if direction == 'D':
-            delta = [-1, 0]
-
-        for x in range(dist):
-            asteps += 1
-            apos = [apos[0] + delta[0], apos[1] + delta[1]]
-            apostup = tuple(apos)
-            if apostup not in aplaces:
-                aplaces[apostup] = asteps
-
-    bpos = [0, 0]
-    bsteps = 0
-
-    for instruction in wires[1].split(','):
-        direction = instruction[0]
-        dist = int(instruction[1:])
-
-        delta = [1, 0]
-
-        if direction == 'R':
-            delta = [0, 1]
-        if direction == 'L':
-            delta = [0, -1]
-        if direction == 'D':
-            delta = [-1, 0]
-
-        for x in range(dist):
-            bsteps += 1
-            bpos = [bpos[0] + delta[0], bpos[1] + delta[1]]
-            bpostup = tuple(bpos)
-            if bpostup not in intersections and bpostup in aplaces:
-                intersections[bpostup] = bsteps + aplaces[bpostup]
-
-    return min(intersections.values())
+    aplaces = wire_to_places(wires[0].split(','))
+    bplaces = wire_to_places(wires[1].split(','))
+    
+    return min(aplaces[k] + bplaces[k] for k in aplaces.keys() & bplaces.keys())
 
 def read_and_solve():
     with open('input_3.txt') as f:
