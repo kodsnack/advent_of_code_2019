1d3d103bc3e426c7820cfff69601147707fd40c9
Erik Dyrelius <erik.dyrelius@gmail.com>
2019-12-03 06:25:04 +0100

    Fullösning

--- Dyr-El-python/day03.py
+++ Dyr-El-python/day03.py
@@ -15,7 +15,29 @@ def fileParse(inp, f=lineParse, ff=lambda x:x, fp=re.compile(r"^(.*)$")):
 ## End of header boilerplate ###################################################
 
 def part1(pinp):
-    return "<solution1>"
+    x, y = 0, 0
+    st = 0
+    sp = {(0, 0):0}
+    for s in pinp[0][0]:
+        d = {'U':(0, 1),'D':(0, -1), 'L': (-1, 0), 'R':(1, 0)}[s[0]]
+        for i in range(int(s[1:])):
+            x, y = x+d[0], y+d[1]
+            st += 1
+            if (x, y) not in sp:
+                sp[(x, y)]=st
+        print(len(sp))
+    xp = set()
+    x, y = 0, 0
+    st = 0
+    for s in pinp[1][0]:
+        d = {'U':(0, 1),'D':(0, -1), 'L': (-1, 0), 'R':(1, 0)}[s[0]]
+        for i in range(int(s[1:])):
+            x, y = x+d[0], y+d[1]
+            st += 1
+            if (x, y) in sp:
+                print(x, y)
+                xp.add(st+sp[(x, y)])
+    return min(xp)
 
 def part2(pinp):
     return "<solution2>"
@@ -24,10 +46,13 @@ def part2(pinp):
 
 if __name__ == "__main__":
     inp = readInput()
-    # inp = """"""
+#     inp = """R75,D30,R83,U83,L12,D49,R71,U7,L72
+# U62,R66,U55,R34,D71,R55,D58,R83"""
+#     inp = """R8,U5,L5,D3
+# U7,R6,D4,L4"""
     
     ## Update for input specifics ##############################################
-    parseInp = fileParse(inp)
+    parseInp = fileParse(inp, ff=lambda x:tuple(x.split(',')))
 
     print("Input is '" + str(parseInp[:10])[:100] + 
           ('...' if len(parseInp)>10 or len(str(parseInp[:10]))>100 else '') + "'")
