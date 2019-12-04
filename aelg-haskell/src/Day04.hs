42fe05485c3b10050d625982c9fe6d9db7e625e7
Linus Mellberg <lmellberg@baffinbaynetworks.com>
2019-12-04 06:32:49 +0100

    Day 4

---
+++ aelg-haskell/src/Day04.hs
@@ -0,0 +1,44 @@
+module Day04
+    ( solve
+    )
+where
+
+import           Control.Arrow
+import           Data.List
+import           Text.ParserCombinators.ReadP
+import qualified Parsing                       as P
+
+range = (,) <$> P.integerAnd (char '-') <*> P.integerAnd eof
+
+parse = P.run range . head
+
+adjacent :: String -> Bool
+adjacent = any ((> 1) . length) . group
+
+adjacentEq :: String -> Bool
+adjacentEq = any ((== 2) . length) . group
+
+increasing [a] = True
+increasing (a : b : xs) | b < a     = False
+                        | otherwise = increasing (b : xs)
+
+solve1 (a, b) =
+    show
+        . length
+        . filter ((== 6) . length)
+        . filter increasing
+        . filter adjacent
+        . map show
+        $ [a .. b]
+
+solve2 (a, b) =
+    show
+        . length
+        . filter ((== 6) . length)
+        . filter increasing
+        . filter adjacentEq
+        . map show
+        $ [a .. b]
+
+solve :: [String] -> (String, String)
+solve = parse ^>> solve1 &&& solve2
