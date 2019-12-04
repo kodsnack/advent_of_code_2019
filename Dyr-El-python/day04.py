c223da0975fb37c43c9442c628b6cd1d08be7b62
Erik Dyrelius <erik.dyrelius@gmail.com>
2019-12-04 08:20:25 +0100

    Shorter solution

--- Dyr-El-python/day04.py
+++ Dyr-El-python/day04.py
@@ -27,39 +27,17 @@ def part1(r1, r2):
     return tot
 
 def test2(s):
-    state = "start"
-    for c in s:
-        if state == "start":
-            success = False
-            newState = "one"
-        elif state == "one":
-            if c > lc:
-                newState = "one"
-            elif c == lc:
-                newState = "two"
-            else:
-                newState = "fail"
-        elif state == "two":
-            if c > lc:
-                success = True
-                newState = "one"
-            elif c == lc:
-                newState = "three"
-            else:
-                newState = "fail"
-        elif state == "three":
-            if c > lc:
-                newState = "one"
-            elif c == lc:
-                newState = "three"
-            else:
-                newState = "fail"
-        if newState == "one":
-            lc = c
-        if newState == "fail":
-            success = False
-        state = newState
-    return state == "two" or success
+    if s != ''.join(sorted(s)):
+        return False
+    for c in set(s):
+        if s.count(c*4)>0:
+            continue
+        if s.count(c*3)==1 and s.count(c*2)==1:
+            continue
+        if s.count(c*2)==0:
+            continue
+        return True
+    return False
 
 def part2(r1, r2):
     tot = 0
