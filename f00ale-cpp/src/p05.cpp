f7540f29196828d22f9eed48f798e8a833e0edbb
Arno Lepisk <arno@lepisk.se>
2019-12-05 21:33:57 +0100

    Day 5 cleanup.

--- f00ale-cpp/src/p05.cpp
+++ f00ale-cpp/src/p05.cpp
@@ -39,79 +39,82 @@ void p05(std::istream & is) {
         return nums;
     }(is);
 
-    auto data = input;
+    auto progdata = [](int prob) { return prob == 1 ? 1 : 5; };
 
-    std::queue<int> q;
-    q.push(5);
+    auto ans = [&](int prob) -> auto & { return prob == 1 ? ans1 : ans2 ;};
 
-    int pos = 0;
-    bool done = false;
-    while (!done) {
-        auto op = data[pos] % 100;
-        auto imm1 = (data[pos] / 100) % 10;
-        auto imm2 = (data[pos] / 1000) % 10;
-        //auto imm3 = (data[pos] / 10000) % 10;
-        switch (op) {
-            case 1:
-                data[data[pos + 3]] = data[imm2 ? pos + 2 : data[pos + 2]] + data[imm1 ? pos + 1 : data[pos + 1]];
-                pos += 4;
-                break;
-            case 2:
-                data[data[pos + 3]] = data[imm2 ? pos+2 : data[pos + 2]] * data[imm1 ? pos+1: data[pos + 1]];
-                pos += 4;
-                break;
-            case 3:
-                if(q.empty()) {
-                    std::cout << "empty queue" << std::endl;
-                    exit(0);
-                }
-                data[data[pos+1]] = q.front();
-                q.pop();
-                pos += 2;
-                break;
-            case 4:
-                std::cout << data[imm1 ? pos+1 : data[pos+1]] << std::endl;
-                pos += 2;
-                break;
-            case 5:
-                if(data[imm1 ?  pos+1 : data[pos+1]]) {
-                    pos = data[imm2 ? pos+2 : data[pos+2]];
-                } else {
-                    pos+= 3;
-                }
-                break;
-            case 6:
-                if(!data[imm1 ?  pos+1 : data[pos+1]]) {
-                    pos = data[imm2 ? pos+2 : data[pos+2]];
-                } else {
-                    pos+= 3;
-                }
-                break;
-            case 7:
-                if(data[imm1 ? pos+1 : data[pos+1]] < data[imm2 ? pos+2 : data[pos+2]]) {
-                    data[data[pos+3]] = 1;
-                } else {
-                    data[data[pos+3]] = 0;
-                }
-                pos += 4;
-                break;
-            case 8:
-                if(data[imm1 ? pos+1 : data[pos+1]] == data[imm2 ? pos+2 : data[pos+2]]) {
-                    data[data[pos+3]] = 1;
-                } else {
-                    data[data[pos+3]] = 0;
-                }
-                pos += 4;
-                break;
-            case 99:
-                done = true;
-                break;
+    for(const auto problem : {1,2}) {
+        auto data = input;
 
-            default:
-                exit(-1);
+        decltype(data)::size_type pos = 0;
+        bool done = false;
+        while (!done) {
+            auto op = data[pos] % 100;
+            auto imm1 = (data[pos] / 100) % 10;
+            auto imm2 = (data[pos] / 1000) % 10;
+            auto imm3 = (data[pos] / 10000) % 10;
+
+            auto a1 = [&]() -> auto & { return data[imm1 ? pos + 1 : data[pos + 1]]; };
+            auto a2 = [&]() -> auto & { return data[imm2 ? pos + 2 : data[pos + 2]]; };
+            auto a3 = [&]() -> auto & { return data[imm3 ? pos + 3 : data[pos + 3]]; };
+
+            switch (op) {
+                case 1:
+                    a3() = a1() + a2();
+                    pos += 4;
+                    break;
+                case 2:
+                    a3() = a1() * a2();
+                    pos += 4;
+                    break;
+                case 3:
+                    a1() = progdata(problem);
+                    pos += 2;
+                    break;
+                case 4:
+                    ans(problem) = a1();
+                    pos += 2;
+                    break;
+                case 5:
+                    if (a1()) {
+                        pos = a2();
+                    } else {
+                        pos += 3;
+                    }
+                    break;
+                case 6:
+                    if (!a1()) {
+                        pos = a2();
+                    } else {
+                        pos += 3;
+                    }
+                    break;
+                case 7:
+                    if (a1() < a2()) {
+                        a3() = 1;
+                    } else {
+                        a3() = 0;
+                    }
+                    pos += 4;
+                    break;
+                case 8:
+                    if (a1() == a2()) {
+                        a3() = 1;
+                    } else {
+                        a3() = 0;
+                    }
+                    pos += 4;
+                    break;
+                case 99:
+                    done = true;
+                    break;
+
+                default:
+                    std::cout << "unknown op " << op << std::endl;
+                    exit(-1);
+            }
         }
     }
-
     std::cout << ans1 << std::endl;
     std::cout << ans2 << std::endl;
 }
