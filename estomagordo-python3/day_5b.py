09a6e2e0517dac604795e36aa91d402613f8f3a5
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2019-12-05 08:25:07 +0100

    bit more compact

--- estomagordo-python3/day_5b.py
+++ estomagordo-python3/day_5b.py
@@ -8,47 +8,37 @@ def solve(d, inp):
     p = 0
 
     while p < len(d):
+        a = -1 if p > len(d) - 2 else d[p + 1] if ((d[p] % 1000) // 100) == 1 else d[d[p + 1]]
+        b = -1 if p > len(d) - 3 else d[p + 2] if ((d[p] % 10000) // 1000) == 1 else -1 if d[p + 2] >= len(d) else d[d[p + 2]]
+
         if d[p] % 100 == 1:
-            a = d[p + 1] if ((d[p] % 1000) // 100) == 1 else d[d[p + 1]]
-            b = d[p + 2] if (((d[p] % 10000) // 1000) == 1) else d[d[p + 2]]
             d[d[p + 3]] = a + b
             p += 4
         elif d[p] % 100 == 2:
-            a = d[p + 1] if ((d[p] % 1000) // 100) == 1 else d[d[p + 1]]
-            b = d[p + 2] if (((d[p] % 10000) // 1000) == 1) else d[d[p + 2]]
             d[d[p + 3]] = a * b
             p += 4
         elif d[p] % 100 == 3:
             d[d[p + 1]] = inp
             p += 2
         elif d[p] % 100 == 4:
-            a = d[p + 1] if ((d[p] % 1000) // 100) == 1 else d[d[p + 1]]
             if a != 0:
                 return a
             p += 2
         elif d[p] % 100 == 5:
-            a = d[p + 1] if ((d[p] % 1000) // 100) == 1 else d[d[p + 1]]
-            b = d[p + 2] if (((d[p] % 10000) // 1000) == 1) else d[d[p + 2]]
             if a != 0:
                 p = b
             else:
                 p += 3
         elif d[p] % 100 == 6:
-            a = d[p + 1] if ((d[p] % 1000) // 100) == 1 else d[d[p + 1]]
-            b = d[p + 2] if (((d[p] % 10000) // 1000) == 1) else d[d[p + 2]]
             if a == 0:
                 p = b
             else:
                 p += 3
         elif d[p] % 100 == 7:
-            a = d[p + 1] if ((d[p] % 1000) // 100) == 1 else d[d[p + 1]]
-            b = d[p + 2] if (((d[p] % 10000) // 1000) == 1) else d[d[p + 2]]
             c = 1 if a < b else 0
             d[d[p + 3]] = c
             p += 4
         elif d[p] % 100 == 8:
-            a = d[p + 1] if ((d[p] % 1000) // 100) == 1 else d[d[p + 1]]
-            b = d[p + 2] if (((d[p] % 10000) // 1000) == 1) else d[d[p + 2]]
             c = 1 if a == b else 0
             d[d[p + 3]] = c
             p += 4
