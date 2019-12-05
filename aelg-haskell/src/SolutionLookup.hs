2575102277f63ea53dce70e3e35d2f69dcc83aa5
Linus Mellberg <lmellberg@baffinbaynetworks.com>
2019-12-05 21:34:29 +0100

    Day 5

--- aelg-haskell/src/SolutionLookup.hs
+++ aelg-haskell/src/SolutionLookup.hs
@@ -9,6 +9,12 @@ import qualified Day01
 import qualified Day02
 import qualified Day03
 import qualified Day04
+import qualified Day05
 
 solutions = M.fromList
-    [(1, Day01.solve), (2, Day02.solve), (3, Day03.solve), (4, Day04.solve)]
+    [ (1, Day01.solve)
+    , (2, Day02.solve)
+    , (3, Day03.solve)
+    , (4, Day04.solve)
+    , (5, Day05.solve)
+    ]
