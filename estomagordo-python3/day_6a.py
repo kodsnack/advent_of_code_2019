355a582dea270fb617f29c48d064419595bac396
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2019-12-06 06:34:32 +0100

    the code that actually solved day 6

--- estomagordo-python3/day_6a.py
+++ estomagordo-python3/day_6a.py
@@ -1,3 +1,4 @@
+import helpers
 import re
 
 from heapq import heappop, heappush
@@ -5,13 +6,38 @@ from collections import Counter, defaultdict
 
 
 def solve(d):
-    pass
-	
+    graph = {}
+
+    for line in d:
+        a, b = line.split(')')
+        graph[b] = a
+        if a not in graph:
+            graph[a] = 'STOP'
+
+    vals = graph.values()
+
+    count = 0
+
+    for node in graph.keys():
+        # if node in vals:
+        #     continue
+
+        orig = node
+
+        runlen = -1
+
+        while node in graph:
+            runlen += 1
+            node = graph[node]
+
+        count += runlen
+
+    return count
 
 def read_and_solve():
-	with open('input_6.txt') as f:
-		data = [line.rstrip() for line in f]
-		return solve(data)
+    with open('input_6.txt') as f:
+        data = [line.rstrip() for line in f]
+        return solve(data)
 
 if __name__ == '__main__':
-	print(read_and_solve())
+    print(read_and_solve())
--- estomagordo-python3/day_6b.py
+++ estomagordo-python3/day_6b.py
@@ -1,3 +1,4 @@
+import helpers
 import re
 
 from heapq import heappop, heappush
@@ -5,13 +6,49 @@ from collections import Counter, defaultdict
 
 
 def solve(d):
-    pass
-	
+    graph = {}
+
+    for line in d:
+        a, b = line.split(')')
+        graph[b] = a
+        if a not in graph:
+            graph[a] = 'STOP'
+
+    youorbits = {}
+    sanorbits = {}
+
+    for node in graph.keys():
+        mode = 0 if node == graph['YOU'] else 1 if node == graph['SAN'] else -1
+
+        if mode == -1:
+            continue
+
+        runlen = 0
+
+        while node in graph:
+            runlen += 1
+            node = graph[node]
+            if mode == 0:
+                youorbits[node] = runlen
+            else:
+                sanorbits[node] = runlen
+
+    best = 10**10
+
+    for k, v in youorbits.items():
+        if k not in sanorbits:
+            continue
+
+        best = min(best, v + sanorbits[k])
+
+    return best
+
+    return count
 
 def read_and_solve():
-	with open('input_6.txt') as f:
-		data = [line.rstrip() for line in f]
-		return solve(data)
+    with open('input_6.txt') as f:
+        data = [line.rstrip() for line in f]
+        return solve(data)
 
 if __name__ == '__main__':
-	print(read_and_solve())
+    print(read_and_solve())
