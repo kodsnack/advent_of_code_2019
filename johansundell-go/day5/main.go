a5d7e2cff6d0bc897112240741c2af3b30229b2b
Johan Sundell <johansundell@gmail.com>
2019-12-06 16:45:26 +0100

    Day 5, part 1 done

--- johansundell-go/day5/main.go
+++ johansundell-go/day5/main.go
@@ -1,7 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/johansundell/advent_of_code_2017/johansundell-go/adventofcode2017"
+)
 
 func main() {
-	fmt.Println(4 % 100)
+	data, _ := adventofcode2017.GetInput("day5.txt")
+	fmt.Println(parseOpcode(data, 0))
+}
+
+func parseOpcode(str string, wantPos int) (answ int) {
+	sArr := strings.Split(str, ",")
+	input := make([]int, len(sArr))
+	for k, s := range sArr {
+		i, _ := strconv.Atoi(s)
+		input[k] = i
+	}
+	for i := 0; ; {
+		operation := input[i] % 100
+
+		switch operation {
+		case 1:
+			//num1, num2, pos := input[i+1], input[i+2], input[i+3]
+			num1, num2, pos := getValues(input, i)
+			//input[pos] = input[num1] + input[num2]
+			input[pos] = num1 + num2
+			i = i + 4
+		case 2:
+			//num1, num2, pos := input[i+1], input[i+2], input[i+3]
+			num1, num2, pos := getValues(input, i)
+			//input[pos] = input[num1] * input[num2]
+			input[pos] = num1 * num2
+			i = i + 4
+		case 3:
+			input[input[i+1]] = 1
+			i = i + 2
+		case 4:
+			fmt.Println(input[input[i+1]])
+			i = i + 2
+		case 99:
+			//answ = input[wantPos]
+			return
+		default:
+			return -1
+		}
+	}
+	return -1
+}
+
+func getValues(input []int, i int) (param1, param2, pos int) {
+	if (input[i]%1000)/100 == 0 {
+		param1 = input[input[i+1]]
+	} else {
+		param1 = input[i+1]
+	}
+
+	if (input[i]%10000)/1000 == 0 {
+		param2 = input[input[i+2]]
+	} else {
+		param2 = input[i+2]
+	}
+
+	if input[i]/10000 == 0 {
+		pos = input[i+3]
+	}
+
+	return
 }
