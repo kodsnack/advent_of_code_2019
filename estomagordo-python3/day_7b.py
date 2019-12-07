b0c5ff2670e60f0fd38261e90b37e74a6e743a61
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2019-12-07 09:08:20 +0100

    reasonable 7b

--- estomagordo-python3/day_7b.py
+++ estomagordo-python3/day_7b.py
@@ -5,8 +5,8 @@ from collections import Counter, defaultdict
 from itertools import permutations
 
 
-def solve_amp(d, p, inp_a, inp_b, inpcount):
-    inp = [inp_a, inp_b]
+def solve_amp(d, p, inputs):
+    inp_pos = 0
 
     while p < len(d):
         a = -1 if p > len(d) - 2 else d[p + 1] if ((d[p] % 1000) // 100) == 1 else d[d[p + 1]]
@@ -21,8 +21,8 @@ def solve_amp(d, p, inp_a, inp_b, inpcount):
             d[d[p + 3]] = a * b
             p += 4
         elif d[p] % 100 == 3:
-            d[d[p + 1]] = inp[inpcount]
-            inpcount = 1
+            d[d[p + 1]] = inputs[inp_pos]
+            inp_pos = 1
             p += 2
         elif d[p] % 100 == 4:
             if a != 0:
@@ -48,21 +48,25 @@ def solve_amp(d, p, inp_a, inp_b, inpcount):
             p += 4
 
 
-def solve(d):
+def solve(d, lo, hi, ampcount):
     best = 0
 
-    for perm in permutations(range(5, 10)):
-        ds = [list(d) for _ in range(5)]
-        poss = [0 for _ in range(5)]
+    for perm in permutations(range(lo, hi)):
+        ds = [list(d) for _ in range(ampcount)]
+        pointers = [0 for _ in range(ampcount)]
         val = 0
 
         pos = 0
         prev = 0
         
         while True:
-            val, dp, p, finished = solve_amp(ds[pos % 5], poss[pos % 5], perm[pos % 5], val, 0 if pos < 5 else 1)
-            ds[pos % 5] = dp
-            poss[pos % 5] = p
+            data = ds[pos % ampcount]
+            pointer = pointers[pos % ampcount]
+            inputs = (perm[pos] if pos < ampcount else val, val)
+            val, dp, p, finished = solve_amp(data, pointer, inputs)
+
+            ds[pos % ampcount] = dp
+            pointers[pos % ampcount] = p
             if finished:
                 best = max(best, prev)
                 break
@@ -75,7 +79,7 @@ def solve(d):
 def read_and_solve():
     with open('input_7.txt') as f:
         data = list(map(int, f.readline().split(',')))
-        return solve(data)
+        return solve(data, 5, 10, 5)
 
 if __name__ == '__main__':
     print(read_and_solve())
