fea7b72c05fcf8c0610bbcaed286f2f27857a37f
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2019-12-07 09:08:56 +0100

    day 7a is basically just 7b with two params changed

--- estomagordo-python3/day_7a.py
+++ estomagordo-python3/day_7a.py
@@ -4,58 +4,16 @@ from heapq import heappop, heappush
 from collections import Counter, defaultdict
 from itertools import permutations
 
-def first(d, inp):
-    p = 0
 
-    while p < len(d):
-        a = -1 if p > len(d) - 2 else d[p + 1] if ((d[p] % 1000) // 100) == 1 else d[d[p + 1]]
-        b = -1 if p > len(d) - 3 else d[p + 2] if ((d[p] % 10000) // 1000) == 1 else -1 if d[p + 2] >= len(d) else d[d[p + 2]]
-
-        if d[p] % 100 == 1:
-            d[d[p + 3]] = a + b
-            p += 4
-        elif d[p] % 100 == 2:
-            d[d[p + 3]] = a * b
-            p += 4
-        elif d[p] % 100 == 3:
-            d[d[p + 1]] = inp
-            p += 2
-        elif d[p] % 100 == 4:
-            if a != 0:
-                return d
-            p += 2
-        elif d[p] % 100 == 5:
-            if a != 0:
-                p = b
-            else:
-                p += 3
-        elif d[p] % 100 == 6:
-            if a == 0:
-                p = b
-            else:
-                p += 3
-        elif d[p] % 100 == 7:
-            c = 1 if a < b else 0
-            d[d[p + 3]] = c
-            p += 4
-        elif d[p] % 100 == 8:
-            c = 1 if a == b else 0
-            d[d[p + 3]] = c
-            p += 4
-
-
-def solve_amp(d, inp_a, inp_b):
-    # d = first(d, inp_a)
-    inp = [inp_a, inp_b]
-    inpcount = 0
-    p = 0
+def solve_amp(d, p, inputs):
+    inp_pos = 0
 
     while p < len(d):
         a = -1 if p > len(d) - 2 else d[p + 1] if ((d[p] % 1000) // 100) == 1 else d[d[p + 1]]
         b = -1 if p > len(d) - 3 else d[p + 2] if ((d[p] % 10000) // 1000) == 1 else -1 if d[p + 2] >= len(d) else d[d[p + 2]]
 
         if d[p] % 100 == 99:
-            return d[0]
+            return d[0], d, 0, True
         if d[p] % 100 == 1:
             d[d[p + 3]] = a + b
             p += 4
@@ -63,12 +21,12 @@ def solve_amp(d, inp_a, inp_b):
             d[d[p + 3]] = a * b
             p += 4
         elif d[p] % 100 == 3:
-            d[d[p + 1]] = inp[inpcount]
-            inpcount = 1
+            d[d[p + 1]] = inputs[inp_pos]
+            inp_pos = 1
             p += 2
         elif d[p] % 100 == 4:
             if a != 0:
-                return a
+                return a, d, p + 2, False
             p += 2
         elif d[p] % 100 == 5:
             if a != 0:
@@ -90,18 +48,30 @@ def solve_amp(d, inp_a, inp_b):
             p += 4
 
 
-def solve(d):
+def solve(d, lo, hi, ampcount):
     best = 0
 
-    for p in permutations(range(5)):
-        val = solve_amp(list(d), p[0], 0)
-        print(val)
+    for perm in permutations(range(lo, hi)):
+        ds = [list(d) for _ in range(ampcount)]
+        pointers = [0 for _ in range(ampcount)]
+        val = 0
 
-        for x in range(1, 5):
-            val = solve_amp(list(d), p[x], val)
-            print(val)
+        pos = 0
+        prev = 0
+        
+        while True:
+            data = ds[pos % ampcount]
+            pointer = pointers[pos % ampcount]
+            inputs = (perm[pos] if pos < ampcount else val, val)
+            val, dp, p, finished = solve_amp(data, pointer, inputs)
 
-        best = max(best, val)
+            ds[pos % ampcount] = dp
+            pointers[pos % ampcount] = p
+            if finished:
+                best = max(best, prev)
+                break
+            prev = val
+            pos += 1
 
     return best
 
@@ -109,7 +79,7 @@ def solve(d):
 def read_and_solve():
     with open('input_7.txt') as f:
         data = list(map(int, f.readline().split(',')))
-        return solve(data)
+        return solve(data, 0, 5, 5)
 
 if __name__ == '__main__':
     print(read_and_solve())
