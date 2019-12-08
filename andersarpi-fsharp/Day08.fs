498fa3ca7ce9ead2ddc8bab5169ac5033e28e4ab
Anders Arpi <anders@adfenix.com>
2019-12-08 10:05:17 +0100

    day08 cleanup

--- andersarpi-fsharp/Day08.fs
+++ andersarpi-fsharp/Day08.fs
@@ -2,25 +2,24 @@
 
 let part1 () = 
     let input = System.IO.File.ReadAllText("day08input.txt")
-
     let layer = input.ToCharArray()
-                |> Seq.ofArray
                 |> Seq.chunkBySize 150
                 |> Seq.map (Seq.countBy id >> Map.ofSeq)
-                |> Seq.minBy (fun x -> x.['0'])
+                |> Seq.minBy (Map.find '0')
 
-    layer.['1'] * layer.['2']
+    layer.['1'] * layer.['2'] |> printfn "%i"
 
 let part2 () =
     let input = System.IO.File.ReadAllText("day08input.txt")
-    
     let layers = input.ToCharArray() |> Seq.chunkBySize 150
 
     let image = [
         for i in 0..149 ->
             layers
             |> Seq.find (fun l -> l.[i] <> '2')
-            |> (fun l -> if l.[i] = '1' then '#' else ' ')
+            |> (fun l -> if l.[i] = '1' then '█' else ' ')
     ]
 
-    image |> Seq.chunkBySize 25 |> Seq.iter (System.String >> printfn "%s")
+    image
+    |> Seq.chunkBySize 25
+    |> Seq.iter (System.String >> printfn "%s")
