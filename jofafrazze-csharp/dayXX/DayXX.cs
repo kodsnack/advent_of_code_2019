2ee3857e0557af98802b1d2fe071d99de6c04a9a
jofafrazze <johan@hallonbrax.com>
2019-12-09 11:28:41 +0100

    OK, I'm really late here, but better late than never, right!?

---
+++ jofafrazze-csharp/dayXX/DayXX.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace dayXX
+{
+    class DayXX
+    {
+        static int[] ReadInput()
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"..\..\..\input.txt");
+            StreamReader reader = File.OpenText(path);
+            int[] data = { 0, 1, 2 };
+            //List<int[]> list = new List<int[]>();
+            //string line;
+            //while ((line = reader.ReadLine()) != null)
+            //{
+            //    int[] d = line.Split('x').Select(int.Parse).ToArray();
+            //    list.Add(d);
+            //}
+            return data;
+        }
+
+        static void PartA()
+        {
+            Console.WriteLine("Part A: Result is {0}.", 0);
+        }
+
+        static void PartB()
+        {
+            Console.WriteLine("Part B: Result is {0}.", 0);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AoC 2019 - " + typeof(DayXX).Namespace + ":");
+            PartA();
+            PartB();
+        }
+    }
+}
