fcf8b2aa39d7e3677cfffcaef1a86b86f2d8d3b4
Arno Lepisk <arno@lepisk.se>
2019-12-09 21:40:26 +0100

    Refactor: break out intcode.

--- f00ale-cpp/src/p09.cpp
+++ f00ale-cpp/src/p09.cpp
@@ -1,140 +1,22 @@
 #include <iostream>
-#include <vector>
-#include <tuple>
-#include <queue>
-#include <algorithm>
-#include <numeric>
-#include <sstream>
+
+#include "intcode.h"
 
 void p09(std::istream & is) {
     int64_t ans1 = 0;
     int64_t ans2 = 0;
-    const std::vector<int64_t> input = [](auto &is) {
-        bool done = false;
-        int64_t num = 0;
-        bool have_num = false;
-        bool neg = false;
-        std::vector<int64_t> nums;
-        while (!done) {
-            char c;
-            is.get(c);
-            if (!is.good()) {
-                done = true;
-                c = '\n';
-            }
 
-            if(c >= '0' && c <= '9') {
-                num *= 10;
-                num += c - '0';
-                have_num = true;
-            } else if (c == '-' && !have_num) {
-                neg = true;
-            } else {
-                if (have_num) {
-                    nums.push_back(neg ? -num : num);
-                }
-                neg = false;
-                have_num = false;
-                num = 0;
-            }
+    const auto input = readIntcode(is);
 
-        }
-        return nums;
-    }(is);
+    auto ans = [&](int prob) -> auto & { return prob == 1 ? ans1 : ans2 ;};
 
-    auto ans = [&](int prob) -> auto & { return prob == 0 ? ans1 : ans2 ;};
-    for(int problem = 0; problem < 2; problem++) {
-        auto data = input;
-        auto pos = 0;
-        auto rel = 0;
-        std::queue<int> inputqueue;
-        inputqueue.emplace(problem + 1);
-        bool done = false;
-
-        while (!done) {
-            const auto op = data[pos] % 100;
-            const auto imm1 = ((data[pos] / 100) % 10) == 1;
-            const auto imm2 = ((data[pos] / 1000)) % 10 == 1;
-            const auto imm3 = ((data[pos] / 10000)) % 10 == 1;
-            const auto rel1 = ((data[pos] / 100)) % 10 == 2;
-            const auto rel2 = ((data[pos] / 1000)) % 10 == 2;
-            const auto rel3 = ((data[pos] / 10000)) % 10 == 2;
-
-            auto getdata = [&](size_t addr) -> auto & {
-                if(addr >= data.size()) data.resize(addr+1, 0);
-                return data[addr];
-            };
-            auto a1 = [&]() -> auto & { return getdata(imm1 ? pos + 1 : (rel1?rel:0)+getdata(pos+1)); };
-            auto a2 = [&]() -> auto & { return getdata(imm2 ? pos + 2 : (rel2?rel:0)+getdata(pos+2)); };
-            auto a3 = [&]() -> auto & { return getdata(imm3 ? pos + 3 : (rel3?rel:0)+getdata(pos+3)); };
-
-            switch (op) {
-                case 1:
-                    a3() = a1() + a2();
-                    pos += 4;
-                    break;
-                case 2:
-                    a3() = a1() * a2();
-                    pos += 4;
-                    break;
-                case 3:
-                    if (inputqueue.empty()) {
-                        std::cout << "nothing in inputqueue" << std::endl;
-                        exit(0);
-                    }
-                    a1() = inputqueue.front();
-                    inputqueue.pop();
-                    pos += 2;
-                    break;
-                case 4:
-                    ans(problem) = a1();
-                    pos += 2;
-                    break;
-                case 5:
-                    if (a1()) {
-                        pos = a2();
-                    } else {
-                        pos += 3;
-                    }
-                    break;
-                case 6:
-                    if (!a1()) {
-                        pos = a2();
-                    } else {
-                        pos += 3;
-                    }
-                    break;
-                case 7:
-                    if (a1() < a2()) {
-                        a3() = 1;
-                    } else {
-                        a3() = 0;
-                    }
-                    pos += 4;
-                    break;
-                case 8:
-                    if (a1() == a2()) {
-                        a3() = 1;
-                    } else {
-                        a3() = 0;
-                    }
-                    pos += 4;
-                    break;
-                case 9:
-                    rel += a1();
-                    pos += 2;
-                    break;
-                case 99:
-                    done = true;
-                    break;
-
-                default:
-                    std::cout << "unknown op " << op << std::endl;
-                    exit(-1);
-            }
-        }
-
+    for(int problem : {1,2}) {
+        intcodemachine mach(input);
+        mach.addInput(problem);
+        while(mach.step());
+        ans(problem) = mach.output;
     }
+
     std::cout << ans1 << std::endl;
     std::cout << ans2 << std::endl;
 }
