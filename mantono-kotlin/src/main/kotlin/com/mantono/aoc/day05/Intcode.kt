d316f38b27c92b026a39a3111d5e16a50e0d4f66
Anton Österberg <anton.osterberg@gmail.com>
2019-12-11 09:08:18 +0100

    Add some printing for debugging purposes

--- mantono-kotlin/src/main/kotlin/com/mantono/aoc/day05/Intcode.kt
+++ mantono-kotlin/src/main/kotlin/com/mantono/aoc/day05/Intcode.kt
@@ -23,18 +23,20 @@ enum class OpCode(
         mem[input.pop()] = i0 + i1
     }),
     MULT(2, 3, { mem, input, _ ->
-        val modes: kotlin.collections.List<com.mantono.aoc.day05.Mode> = com.mantono.aoc.day05.Mode.parse(input.pop())
-        val i0: kotlin.Int = mem.read(modes[0], input.pop())
-        val i1: kotlin.Int = mem.read(modes[1], input.pop())
+        val modes: List<Mode> = Mode.parse(input.pop())
+        val i0: Int = mem.read(modes[0], input.pop())
+        val i1: Int = mem.read(modes[1], input.pop())
         mem[input.pop()] = i0 + i1
     }),
     STORE(3, 1, { mem, input, addr ->
-        // TODO FIX Me!
-        mem[mem[addr]] = mem[addr+1]
+        val modes: List<Mode> = Mode.parse(input.pop())
+        val i0: Int = mem.read(modes[0], input.pop())
+        mem[i0] = i0
     }),
     READ(4, 2, { mem, input, addr ->
-        // TODO FIX Me!
-        mem[addr+2]
+        val modes: List<Mode> = Mode.parse(input.pop())
+        val i0: Int = mem.read(modes[0], input.pop())
+        mem[addr+2] = i0
     }),
     HALT(99, 0, { mem, _, _ ->
         mem[0]
@@ -73,20 +75,29 @@ fun Memory.read(mode: Mode, address: Int): Int {
 
 @AoC(5, Part.A)
 fun intCode(input: String): Int {
-    input.split(",").map { it.trim().toInt() }
-        .forEach { if(it in 1..4) print("\n$it, ") else print("$it, ") }
+//    input.split(",").map { it.trim().toInt() }
+//        .forEach { if(it in 1..4) print("\n$it, ") else print("$it, ") }
     val memory: MutableList<Int> = input.split(",")
         .map { it.trim().toInt() }
         .toMutableList()
+
     return parseData(memory)
 }
 
 tailrec fun parseData(memory: MutableList<Int>, index: Int = 0): Int {
+    Thread.sleep(1500L)
     val opCode = OpCode.parse(memory[index])
     val from: Int = index
     val to: Int = from + opCode.inputs
     val instructions: Instruction = LinkedList(memory[from..to])
-    //val input: List<Int> = readDataInMemory(memory, modes, index, opCode.inputs)
+    val line: String = memory.mapIndexed { i, n ->
+        when {
+            i == from -> "[$n"
+            i == to -> "$n]"
+            else -> n.toString()
+        }
+    }.joinToString(separator = ", ") { it.toString() }
+    println(line)
     opCode.execute(memory, instructions, index)
     val address: Int = to + 1
     if(opCode == OpCode.HALT) {
