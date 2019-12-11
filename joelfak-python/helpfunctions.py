904f2bc601b09ac6035d10daed67ca41ba73b6be
Joel Fåk <joel.fak@niradynamics.se>
2019-12-11 13:41:29 +0100

    Day 2

--- joelfak-python/helpfunctions.py
+++ joelfak-python/helpfunctions.py
@@ -9,4 +9,9 @@ def readFile(filename):
             yield data.strip()
 
 def getIntsFromFile(filename):
-    return  map(lambda x: int(x), readFile(filename))
+    return map(lambda x: int(x), readFile(filename))
+
+def getCommaSeparatedIntsFromFile(filename):
+    with open(filename) as file:
+        str = file.readline()
+    return list(map(int, str.split(",")))
