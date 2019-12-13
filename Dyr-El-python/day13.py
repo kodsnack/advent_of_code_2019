6b3584dc294aeead787d596b12829d8380f65d28
Erik Dyrelius <erik.dyrelius@scania.com>
2019-12-13 15:09:18 +0100

    Added terminal gui

--- Dyr-El-python/day13.py
+++ Dyr-El-python/day13.py
@@ -2,6 +2,8 @@
 
 from aocbase import readInput
 import re
+import curses
+import time
 
 def lineParse(s, f, fp):
     m = fp.match(s)
@@ -189,36 +191,68 @@ def part1(pinp):
             tot += 1
     return tot
 
-def runPart(c):
+def initScr(cs):
+    if cs:
+        cs.nodelay(True)
+        curses.curs_set(0)
+    return dict()
+
+def updatePos(cs, screen, x, y, c):
+    screen[x, y] = c
+    if cs:
+        cs.addstr(y, x, c)
+
+def updateScore(cs, screen, score):
+    screen["score"] = score
+    if cs:
+        cs.addstr(1, 45, "Score: {}".format(score))
+
+lastTime = None
+def updateScreen(cs):
+    global lastTime
+    if cs:
+        c = cs.getch()
+        now = time.time()
+        if lastTime:
+            time.sleep(max(0.1 - (now - lastTime), 0))
+        lastTime = now
+        if 0 < c < 256:
+            return chr(c)
+    return ""
+
+def runPart(c, scr):
     screen = dict()
+    screen = initScr(scr)
     xmin,xmax,ymin,ymax = 0, 0, 0, 0
     nofblocks = 0
     paddlex = 0
     ballx = 0
     while c.state != "halted":
         c.run()
+        if updateScreen(scr)=='Q':
+            break
         while not c.outEmpty():
             x = c.out()
             y = c.out()
             t = c.out()
             if x == -1 and y == 0:
-                screen["score"] = t
+                updateScore(scr, screen, t)
                 continue
             elif t == 0:
                 if screen.get((x, y), ' ')  == '#':
                     nofblocks -= 1
-                screen[x, y] = ' '
+                updatePos(scr, screen, x, y, ' ')
             elif t == 1:
-                screen[x, y] = '|'
+                updatePos(scr, screen, x, y, '|')
             elif t == 2:
-                screen[x, y] = '#'
+                updatePos(scr, screen, x, y, '#')
                 nofblocks += 1
             elif t == 3:
                 paddlex = x
-                screen[x, y] = '-'
+                updatePos(scr, screen, x, y, '=')
             elif t == 4:
                 ballx = x
-                screen[x, y] = '*'
+                updatePos(scr, screen, x, y, 'O')
             xmin = min(x, xmin)
             xmax = max(x, xmax)
             ymin = min(y, ymin)
@@ -233,14 +267,14 @@ def runPart(c):
             c.inp(0)
     return screen["score"]
 
-def part2(pinp):
+def part2(pinp, sc):
     c = Comp(pinp[0][0])
     c.mem[0] = 2
-    return runPart(c)
+    return runPart(c, sc)
 
 ## Start of footer boilerplate #################################################
 
-if __name__ == "__main__":
+def main(stdscreen, ):
     inp = readInput()
     # inp = """"""
     
@@ -249,7 +283,15 @@ if __name__ == "__main__":
 
     print("Input is '" + str(parseInp[:10])[:100] + 
           ('...' if len(parseInp)>10 or len(str(parseInp[:10]))>100 else '') + "'")
+
     print("Solution to part 1: {}".format(part1(parseInp)))
-    print("Solution to part 2: {}".format(part2(parseInp)))
+    print("Solution to part 2: {}".format(part2(parseInp, stdscreen)))
+
+import sys
+if __name__ == "__main__":
+    if "gui" in sys.argv:
+        curses.wrapper(main)
+    else:
+        main(None)
 
 ## End of footer boilerplate ###################################################
