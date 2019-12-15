54718c475f57124446b131c89212a1e9239169c0
Erik Dyrelius <erik.dyrelius@gmail.com>
2019-12-15 23:33:10 +0100

    Nim solution for day 10 finally in place

--- Dyr-El-nim/src/day10.nim
+++ Dyr-El-nim/src/day10.nim
@@ -1,5 +1,7 @@
 import sets
 import strutils
+import sequtils
+import algorithm
 
 type
     Point = tuple[x:int, y: int]
@@ -53,5 +55,68 @@ proc findBestAsteroid*(theMap: string): int =
                 p = p + vector
             if not hidden:
                 seen.inc
-        echo("Asteroid ", baseAsteroid, " seen: ", seen)
         result = max(result, seen)
+
+proc findBestAsteroidCoords(asteroids: PointSet): Point =
+    var bestSeen = 0
+    for baseAsteroid in asteroids:
+        var seen = 0
+        for otherAsteroid in asteroids:
+            if baseAsteroid == otherAsteroid:
+                continue
+            let
+                dist = otherAsteroid - baseAsteroid
+                vector = dist.norm
+            var
+                hidden = false
+                p = baseAsteroid + vector
+            while not hidden and p != otherAsteroid:
+                hidden = hidden or asteroids.contains(p)
+                p = p + vector
+            if not hidden:
+                seen.inc
+        if seen > bestSeen:
+            bestSeen = seen
+            result = baseAsteroid
+
+proc sweepOrder(v: Point): float64 =
+    result = v.y * abs(v.y) / (v.x * v.x + v.y * v.y)
+    if v.x < 0:
+        result = 2 - result
+
+proc findMin(ps: PointSet): Point =
+    for p in ps:
+        result.x = min(result.x, p.x)
+        result.y = min(result.y, p.y)
+
+proc findMax(ps: PointSet): Point =
+    for p in ps:
+        result.x = max(result.x, p.x)
+        result.y = max(result.y, p.y)
+        
+proc numberVaporised*(theMap: string, number: int): int =
+    var asteroids = parseAsteroidMap(theMap)
+    let minPoint = findMin(asteroids)
+    let maxPoint = findMax(asteroids)
+    let baseAsteroid = findBestAsteroidCoords(asteroids)
+    asteroids.excl(baseAsteroid)
+    var vectorSet: PointSet
+    vectorSet.init()
+    for a in asteroids:
+        vectorSet.incl((a - baseAsteroid).norm())
+    var vectorList: Points = toSeq(vectorSet.items())
+    vectorList.sort do (v1, v2: Point) -> int:
+        result = cmp(v1.sweepOrder, v2.sweepOrder)
+    var killed = 0
+    while true:
+        for v in vectorList:
+            var p = baseAsteroid
+            while (p.x >= minPoint.x and p.x <= maxPoint.x and 
+                   p.y >= minPoint.y and p.y <= maxPoint.y):
+                p = p + v
+                if p in asteroids:
+                    asteroids.excl(p)
+                    killed.inc
+                    if killed == number:
+                        return p.x*100+p.y
+                    break
