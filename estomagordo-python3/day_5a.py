6600434b555aa3f1e60ae30d395556f6b54f4d4a
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2019-12-16 14:27:16 +0100

    day 5 uses intcode Computer

--- estomagordo-python3/day_5a.py
+++ estomagordo-python3/day_5a.py
@@ -2,29 +2,21 @@ import re
 
 from heapq import heappop, heappush
 from collections import Counter, defaultdict
+from intcode import Computer
 
 
 def solve(d, inp):
-    p = 0
+    computer = Computer(d, inp)
+    answer = 0
+    
+    while True:
+        retcode, retval = computer.get_output()
 
-    while p < len(d):
-        a = -1 if p > len(d) - 2 else d[p + 1] if ((d[p] % 1000) // 100) == 1 else d[d[p + 1]]
-        b = -1 if p > len(d) - 3 else d[p + 2] if ((d[p] % 10000) // 1000) == 1 else -1 if d[p + 2] >= len(d) else d[d[p + 2]]
-
-        if d[p] % 100 == 1:
-            d[d[p + 3]] = a + b
-            p += 4
-        elif d[p] % 100 == 2:
-            d[d[p + 3]] = a * b
-            p += 4
-        elif d[p] % 100 == 3:
-            d[d[p + 1]] = inp
-            p += 2
-        elif d[p] % 100 == 4:
-            if a != 0:
-                return a
-            p += 2
-
+        if retcode == 1:
+            answer = retval
+        else:
+            return answer
+            
 
 def read_and_solve():
     with open('input_5.txt') as f:
