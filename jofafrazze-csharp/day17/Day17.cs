905cba1bd431b1aefd2c146ba4f103b8c4e52e19
jofafrazze <johan@hallonbrax.com>
2019-12-16 15:54:58 +0100

    Adding my solutions to day 14 to 16

---
+++ jofafrazze-csharp/day17/Day17.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+//using AdventOfCode;
+//using Position = AdventOfCode.GenericPosition2D<int>;
+
+namespace day17
+{
+    class Day17
+    {
+        static List<int> ReadInput()
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"..\..\..\input.txt");
+            StreamReader reader = File.OpenText(path);
+            List<int> list = new List<int>();
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                list.AddRange(line.Split(',').Select(int.Parse).ToList());
+            }
+            return list;
+        }
+
+        static void PartA()
+        {
+            List<int> input = ReadInput();
+            Console.WriteLine("Part A: Result is {0}", 0);
+        }
+
+        static void PartB()
+        {
+            Console.WriteLine("Part B: Result is {0}", 0);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AoC 2019 - " + typeof(Day17).Namespace + ":");
+            PartA();
+            PartB();
+        }
+    }
+}
