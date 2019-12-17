0f160efe43320887eeaaf36e965e5deccbaeade1
Anton Österberg <anton.osterberg@gmail.com>
2019-12-17 08:58:58 +0100

    Change Memory.read

--- mantono-kotlin/src/main/kotlin/com/mantono/aoc/day05/Intcode.kt
+++ mantono-kotlin/src/main/kotlin/com/mantono/aoc/day05/Intcode.kt
@@ -6,39 +6,61 @@ import java.util.*
 import kotlin.math.pow
 
 typealias Memory = MutableList<Int>
-typealias Execution = (memory: Memory, addr: Int, output: Deque<Int>) -> Int
+typealias Execution = (memory: Memory, addr: Int, input: Deque<Int>, output: Deque<Int>) -> Int
 
 enum class OpCode(
     val code: Int,
     val execute: Execution
 ) {
-    ADD(1 , { mem, addr, _ ->
+    ADD(1 , { mem, addr, _, _ ->
         val modes: List<Mode> = Mode.parse(mem[addr])
-        val i0: Int = mem.read(modes[0], mem[addr+1])
-        val i1: Int = mem.read(modes[1], mem[addr+2])
+        val i0: Int = mem.read(modes[0], addr+1)
+        val i1: Int = mem.read(modes[1], addr+2)
         mem[mem[addr+3]] = i0 + i1
         addr + 4
     }),
-    MULT(2, { mem, addr, _ ->
+    MULT(2, { mem, addr, _, _ ->
         val modes: List<Mode> = Mode.parse(mem[addr])
-        val i0: Int = mem.read(modes[0], mem[addr+1])
-        val i1: Int = mem.read(modes[1], mem[addr+2])
+        val i0: Int = mem.read(modes[0], addr+1)
+        val i1: Int = mem.read(modes[1], addr+2)
         mem[mem[addr+3]] = i0 * i1
         addr + 4
     }),
-    INPUT(3, { mem, addr, _ ->
+    INPUT(3, { mem, addr, input, _ ->
         val i = mem[addr+1]
-        mem[i] = 1
+        mem[i] = input.poll()
         addr+2
     }),
-    OUTPUT(4, { mem, addr, output ->
+    OUTPUT(4, { mem, addr, output, _ ->
         val modes: List<Mode> = Mode.parse(mem[addr])
-        val i0: Int = mem.read(modes[0], mem[addr+1])
+        val i0: Int = mem.read(modes[0], addr+1)
         println(i0)
         output.push(i0)
         addr+2
     }),
-    HALT(99, { mem, _, _ ->
+    JUMP_IF_TRUE(5, { mem, addr, _, _ ->
+        if(mem[addr+1] != 0) {
+            val modes: List<Mode> = Mode.parse(mem[addr])
+            mem.read(modes[0], mem[addr+2])
+        } else {
+            addr+3
+        }
+    }),
+    JUMP_IF_FALSE(6, { mem, addr, _, _ ->
+        if(mem[addr+1] == 0) {
+            val modes: List<Mode> = Mode.parse(mem[addr])
+            mem.read(modes[0], mem[addr+2])
+        } else {
+            addr+3
+        }
+    }),
+    LESS_THAN(7, { mem, addr, _, _ ->
+        0
+    }),
+    EQUALS(8, { mem, addr, _, _ ->
+        0
+    }),
+    HALT(99, { mem, _, _, _ ->
         mem[0]
     });
 
@@ -68,30 +90,47 @@ enum class Mode {
 
 fun Memory.read(mode: Mode, address: Int): Int {
     return when(mode) {
-        Mode.Address -> this[address]
-        Mode.Value -> address
+        Mode.Address -> this[this[address]]
+        Mode.Value -> this[address]
     }
 }
 
 @AoC(5, Part.A)
 fun intCode(input: String): Int {
-    val memory: MutableList<Int> = input.split(",")
+    return parseMemory(input, 1)
+}
+
+//@AoC(5, Part.B)
+fun intCodeExtended(input: String): Int {
+    return parseMemory(input, 5)
+}
+
+fun parseMemory(program: String, input: Int): Int {
+    val memory: MutableList<Int> = program.split(",")
         .map { it.trim().toInt() }
         .toMutableList()
 
-    return parseData(memory)
+    return parseData(memory, input = LinkedList(listOf(input)))
 }
 
-tailrec fun parseData(memory: MutableList<Int>, pointer: Int = 0, output: Deque<Int> = LinkedList()): Int {
+tailrec fun parseData(
+    memory: MutableList<Int>,
+    pointer: Int = 0,
+    input: Deque<Int>,
+    output: Deque<Int> = LinkedList()
+): Int {
     Thread.sleep(200L)
     val opCode = OpCode.parse(memory[pointer])
-    val pointerMoved: Int = opCode.execute(memory, pointer, output)
+    val pointerMoved: Int = opCode.execute(memory, pointer, input, output)
     dumpMemory(memory, pointer, pointerMoved)
+    check(pointerMoved in memory.indices) {
+        "Address is outside memory: $pointerMoved"
+    }
     if(opCode == OpCode.HALT) {
         println(output.joinToString { it.toString() })
         return memory[pointerMoved]
     }
-    return parseData(memory, pointerMoved, output)
+    return parseData(memory, pointerMoved, input, output)
 }
 
 fun dumpMemory(memory: Memory, from: Int, to: Int) {
