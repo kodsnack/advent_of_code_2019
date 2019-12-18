8fda463bb260b9ae1ffda416c6e0498c959bcfcc
Arno Lepisk <arno@lepisk.se>
2019-12-18 07:02:31 +0100

    Day 18, still too slow.

--- f00ale-cpp/src/p18.cpp
+++ f00ale-cpp/src/p18.cpp
@@ -52,28 +52,57 @@ void p18(std::istream & is) {
     }
     std::cout << sx << " " << sy << " " << nkeys << std::endl;
 
-    std::queue<std::tuple<int, int, int, std::set<char>, std::set<std::tuple<std::set<char>, std::tuple<int,int>>>>> q;
-    q.emplace(sx,sy,0,std::set<char>(), std::set<std::tuple<std::set<char>, std::tuple<int,int>>>());
-/*
-    auto buildreachable = [](int x, int y, const std::set<char> & keys, const std::vector<std::string<){
-
-    };
-  */
-    while(!q.empty()) {
-        auto [x,y,step,keys,visited] = q.front(); q.pop();
-        if(map[y][x] == '#') continue;
-        if(visited.find(std::tuple(keys, std::tuple(x,y))) != visited.end()) continue;
-        visited.insert(std::tuple(keys, std::tuple(x,y)));
-        if(islower(map[y][x])) keys.insert(map[y][x]);
-        if(keys.size() == nkeys) {
-            ans1 = step;
-            break;
+    auto buildreachable = [](int sx, int sy, const std::set<char> & keys, const std::vector<std::string> & map){
+        std::vector<std::tuple<int, int, char, int>> ret;
+        std::queue<std::tuple<int,int,int>> q;
+        q.emplace(std::tuple(sx,sy,0));
+        std::set<char> found;
+        std::set<std::tuple<int,int>> visited;
+        while(!q.empty()) {
+            auto [x,y,step] = q.front(); q.pop();
+            char c = map[y][x];
+            if(c == '#') continue;
+            if(visited.find(std::tuple(x,y)) != visited.end()) continue;
+            visited.insert(std::tuple(x,y));
+            if(islower(c)) {
+                if(keys.find(map[y][x]) == keys.end() && found.find(c) == found.end()) {
+                    // new key
+                    found.insert(c);
+                    ret.emplace_back(x,y, map[y][x], step);
+                }
+            }
+            if(isupper(map[y][x]) && keys.find(tolower(map[y][x])) == keys.end()) continue;
+            q.emplace(x+1, y, step+1);
+            q.emplace(x-1, y, step+1);
+            q.emplace(x, y-1, step+1);
+            q.emplace(x, y+1, step+1);
         }
-        if(isupper(map[y][x]) && keys.find(tolower(map[y][x])) == keys.end()) continue;
-        q.emplace(x+1, y, step+1, keys, visited);
-        q.emplace(x-1, y, step+1, keys, visited);
-        q.emplace(x, y-1, step+1, keys, visited);
-        q.emplace(x, y+1, step+1, keys, visited);
+        return ret;
+    };
+/*
+    std::set<char> k;
+    auto v = buildreachable(sx,sy,k,map);
+    for(auto & [x,y,k,s] : v) {
+        std::cout << k << " " << s << std::endl;
+    }
+*/
+    std::queue<std::tuple<int, int, int, std::set<char>>> q;
+    q.emplace(sx,sy,0,std::set<char>());
+
+    ans1 = std::numeric_limits<int>::max();
+    while(!q.empty()) {
+        auto [x,y,step,keys] = q.front(); q.pop();
+        //std::cout << step << std::endl;
+        auto reachabe = buildreachable(x,y,keys,map);
+        for(auto [nx, ny, nk, ns] : reachabe) {
+            auto keycopy = keys;
+            keycopy.insert(nk);
+            if(keycopy.size() == nkeys) {
+                ans1 = std::min(step+ns,ans1);
+                std::cout << ans1 << std::endl;
+            }
+            q.emplace(nx,ny,step+ns,keycopy);
+        }
     }
 
     std::cout << ans1 << std::endl;
