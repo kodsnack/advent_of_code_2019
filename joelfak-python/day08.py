81969e9cae60cda1162305a8f03714839834c332
Joel Fåk <joel.fak@niradynamics.se>
2019-12-19 22:43:56 +0100

    Day 8: refactor to use input as a string

--- joelfak-python/day08.py
+++ joelfak-python/day08.py
@@ -3,11 +3,10 @@
 from helpfunctions import *
 import unittest, sys, itertools
 
-def splitNumber(number):
+def splitNumber(numberstring):
     l = []
-    while number > 0:
-        l.append(number % 10)
-        number //= 10
+    for char in numberstring:
+        l.append(int(char))
     return l
 
 def decode(imagedata, width, height):
@@ -20,7 +19,7 @@ def decode(imagedata, width, height):
         for y in range(height):
             image[layer].append([])
             for x in range(width):
-                image[layer][y].append(l.pop())
+                image[layer][y].append(l.pop(0))
         layer += 1
 
     return image
@@ -40,12 +39,12 @@ def part2(data):
 
 ## Unit tests ########################################################
 
-class TestDay08(unittest.TestCase):
+class TestDay08_part1(unittest.TestCase):
     def test_splitNumber(self):
-        self.assertEqual(splitNumber(278),[8,7,2])
+        self.assertEqual(splitNumber("278"),[2,7,8])
 
     def test_decode(self):
-        self.assertEqual(decode(123456789012, 3, 2),
+        self.assertEqual(decode("123456789012", 3, 2),
                          [[[1,2,3],
                            [4,5,6]],
 
@@ -53,13 +52,14 @@ class TestDay08(unittest.TestCase):
                            [0,1,2]]])
 
     def test_errorCheck(self):
-        image = decode(111223567890, 3, 2)
+        image = decode("111223567890", 3, 2)
         self.assertEqual(errorCheck(image), 6)
 
 ## Main ########################################################
 
 if __name__ == '__main__':
 
+
     print("Advent of code day 8")
-    print("Part1 result: {}".format(part1(int(list(getStringsFromFile(sys.argv[1]))[0]))))
+    print("Part1 result: {}".format(part1(list(getStringsFromFile(sys.argv[1]))[0])))
     # print("Part2 result: {}".format(part2(getIntsFromFile(sys.argv[1]))))
