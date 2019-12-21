261b2a664eb3f69e0954906c4d0b1f0eb881f882
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2019-12-21 09:14:51 +0100

    sloppy 21a, 21b wip

--- estomagordo-python3/day_21b.py
+++ estomagordo-python3/day_21b.py
@@ -2,16 +2,65 @@ import re
 
 from heapq import heappop, heappush
 from collections import Counter, defaultdict
+from intcode import Computer
 
 
 def solve(d):
-    pass
-	
+    # instructions =
+    # [
+    #     'AND A T', 
+    #     'AND B T', 
+    #     'AND C T', 
+    #     'AND D T', 
+    #     'AND T T', 
+    #     'AND J T', 
+    # ]
+    
+    inp = """NOT A T
+NOT B J
+OR J T
+NOT C J
+OR J T
+OR T J
+AND D J
+NOT E T
+AND F T
+AND A T
+AND B T
+NOT T T
+AND T J
+RUN
+"""
+    p = 0
+    out = []
+    steps = 0
+
+    computer = Computer(d, ord(inp[p]))
+
+    while True:
+        steps += 1
+        retcode, retval = computer.step()
+
+        if retcode == -1:
+            break
+        
+        if retcode == 0 and retval == 3:
+            p += 1
+            if p < len(inp):
+                computer.set_input(ord(inp[p]))
+
+        if retcode == 1:
+            print(chr(retval), end='')
+            # out.append(retval)
+
+    return steps
+    # return ''.join(map(chr, out))
+    
 
 def read_and_solve():
-	with open('input_21.txt') as f:
-		data = [line.rstrip() for line in f]
-		return solve(data)
+    with open('input_21.txt') as f:
+        data = list(map(int, f.readline().split(',')))
+        return solve(data)
 
 if __name__ == '__main__':
-	print(read_and_solve())
+    print(read_and_solve())
