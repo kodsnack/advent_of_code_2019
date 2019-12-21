b2d9ea566986db404ab1be961081510cc29ceea2
Arno Lepisk <arno@lepisk.se>
2019-12-22 00:01:29 +0100

    Day 20, somewhat cleaned up.

--- f00ale-cpp/src/p20.cpp
+++ f00ale-cpp/src/p20.cpp
@@ -14,8 +14,6 @@ void p20(std::istream & is) {
 
     {
         bool done = false;
-        int num = 0;
-        bool have_num = false;
         std::string str;
 
         while (!done) {
@@ -36,18 +34,15 @@ void p20(std::istream & is) {
         }
     }
 
-    for(auto & s : map) std::cout << s << std::endl;
-
     std::map<std::array<char,2>, std::tuple<int,int>> endpointstmp;
 
     std::map<std::tuple<int,int>, std::tuple<int,int>> portals;
     std::map<std::tuple<int,int>,std::array<char,2>> portalnames;
 
-    for(int y = 0; y < map.size(); y++) {
-        for(int x = 0; x < map[y].size(); x++) {
-            if(y+1 < map.size()) if(isupper(map[y][x]) && isupper(map[y+1][x])) {
+    for(size_t y = 0; y < map.size(); y++) {
+        for(size_t x = 0; x < map[y].size(); x++) {
+            if(y+1 < map.size() && isupper(map[y][x]) && isupper(map[y+1][x])) {
                 // vertical portal
-                //std::cout << x << " " << y << "," << y+1 << " " << map[y][x]<<map[y+1][x] <<std::endl;
                 int ry;
                 if(y > 0 && map[y-1][x] == '.') ry = y-1;
                 else if(y+2 < map.size() && map[y+2][x] == '.') ry = y+2;
@@ -57,7 +52,6 @@ void p20(std::istream & is) {
                 auto it = endpointstmp.find(n);
                 if(it != endpointstmp.end()) {
                     auto [ox,oy] = it->second;
-                    //std::cout << x << ',' << ry << " - " << ox << ',' << oy << std::endl;
                     endpointstmp.erase(it);
                     portals.emplace(std::tuple(x,ry), std::tuple(ox,oy));
                     portals.emplace(std::tuple(ox,oy), std::tuple(x,ry));
@@ -65,9 +59,8 @@ void p20(std::istream & is) {
                     endpointstmp.emplace(n, std::tuple(x,ry));
                 }
             }
-            if(x+1 < map[y].size()) if(isupper(map[y][x]) && isupper(map[y][x+1])) {
+            if(x+1 < map[y].size() && isupper(map[y][x]) && isupper(map[y][x+1])) {
                 // horizontal portal
-                //std::cout << x << "," << x+1 << " " << y << " " << map[y][x]<<map[y][x+1] <<std::endl;
                 int rx;
                 if(x > 0 && map[y][x-1] == '.') rx = x-1;
                 else if(x+2 < map[y].size() && map[y][x+2] == '.') rx = x+2;
@@ -78,7 +71,6 @@ void p20(std::istream & is) {
                 auto it = endpointstmp.find(n);
                 if(it != endpointstmp.end()) {
                     auto [ox,oy] = it->second;
-                    //std::cout << rx << ',' << y << " - " << ox << ',' << oy << std::endl;
                     endpointstmp.erase(it);
                     portals.emplace(std::tuple(rx,y), std::tuple(ox,oy));
                     portals.emplace(std::tuple(ox,oy), std::tuple(rx,y));
@@ -91,56 +83,49 @@ void p20(std::istream & is) {
     }
 
     auto start = endpointstmp[std::array<char,2>{'A','A'}];
-    auto [sx,sy] = start;
-    std::cout << sx << ',' << sy << std::endl;
     auto goal = endpointstmp[std::array<char,2>{'Z','Z'}];
 
     for(auto [t1,t2] : portalnames) {
         auto [x,y] = t1;
         map[y][x] = 'P';
     }
-    for(auto & s : map) std::cout << s << std::endl;
 
-    std::queue<std::tuple<std::tuple<int,int>, int, bool, int>> q;
-    q.emplace(std::tuple(start, 0, true, 0));
-    std::set<std::tuple<int,int,int>> visited;
-    while(!q.empty()) {
-        auto [p, step, teleported, level] = q.front();
-        q.pop();
-        if(level < 0) continue;
-        auto [x,y] = p;
-        if(p == goal && level == 0) {
-            ans1 = step;
-            break;
-        }
-        auto pp = std::tuple(x,y,level);
-        if(visited.count(pp)) continue;
-        visited.emplace(pp);
+    for(auto problem : {1,2}) {
+        std::queue<std::tuple<std::tuple<int, int>, int, bool, int>> q;
+        q.emplace(std::tuple(start, 0, true, 0));
+        std::set<std::tuple<int, int, int>> visited;
+        while (!q.empty()) {
+            auto[p, step, teleported, level] = q.front();
+            q.pop();
+            if (level < 0) continue;
+            auto[x, y] = p;
+            if (p == goal && level == 0) {
+                (problem == 1 ? ans1 : ans2) = step;
+                break;
+            }
+            auto pp = std::tuple(x, y, level);
+            if (visited.count(pp)) continue;
+            visited.emplace(pp);
 
-        if(map[y][x] == '.' || teleported) {
-            q.emplace(std::tuple(x+1,y), step+1, false, level);
-            q.emplace(std::tuple(x-1,y), step+1, false, level);
-            q.emplace(std::tuple(x,y+1), step+1, false, level);
-            q.emplace(std::tuple(x,y-1), step+1, false, level);
-        } else if(map[y][x]=='P') {
-            // portal
-            auto it = portals.find(p);
-            if(it == portals.end()) {
-                //std::cout << "portal endpoint not found" << std::endl;
-            } else {
-                int ld = 0;
-                if(x == 2 || x == map[y].size()-3 || y == 2 || y == map.size()-3) ld = -1;
-                else ld = 1;
-                q.emplace(it->second, step+1, true, level+ld);
-                auto [nx,ny] = it->second;
-
-                auto fn = portalnames[p];
-
-                std::cout << " teleport " << x << ',' << y << " -> " << nx << ',' << ny << " " << " " <<  fn[0] << fn[1] << " " << level+ld << std::endl;
+            if (map[y][x] == '.' || teleported) {
+                q.emplace(std::tuple(x + 1, y), step + 1, false, level);
+                q.emplace(std::tuple(x - 1, y), step + 1, false, level);
+                q.emplace(std::tuple(x, y + 1), step + 1, false, level);
+                q.emplace(std::tuple(x, y - 1), step + 1, false, level);
+            } else if (map[y][x] == 'P') {
+                // portal
+                auto it = portals.find(p);
+                if (it != portals.end()) {
+                    int ld = 0;
+                    if(problem == 2) {
+                        if (x == 2 || x == static_cast<int>(map[y].size()) - 3 || y == 2 || y == static_cast<int>(map.size()) - 3) ld = -1;
+                        else ld = 1;
+                    }
+                    q.emplace(it->second, step + 1, true, level + ld);
+                }
             }
         }
     }
-
     std::cout << ans1 << std::endl;
     std::cout << ans2 << std::endl;
 }
