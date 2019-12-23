57e08b3a06ccd85d71ea2f5b4a2449609c9923bc
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2019-12-23 06:46:23 +0100

    23a

--- estomagordo-python3/day_23a.py
+++ estomagordo-python3/day_23a.py
@@ -1,17 +1,50 @@
 import re
 
 from heapq import heappop, heappush
-from collections import Counter, defaultdict
+from collections import Counter, defaultdict, deque
+from intcode import Computer
 
 
 def solve(d):
-    pass
-	
+    size = 50
+    computers = [Computer(d, i) for i in range(size)]
+    outbound = [[] for i in range(size)]
+    inbound = [deque() for i in range(size)]
+
+    while True:
+        for i in range(50):
+            retcode, retval = computers[i].step()
+            
+            if retcode == 1:
+                outbound[i].append(retval)
+                if len(outbound[i]) == 3:
+                    if outbound[i][0] == 255:
+                        return retval
+
+                    destination, x, y = outbound[i]
+
+                    if len(inbound[destination]) == 0 and computers[destination].input == -1:
+                        computers[destination].set_input(x)
+                    else:
+                        inbound[destination].append(x)
+                        
+                    inbound[destination].append(y)
+
+                    outbound[i] = []
+            elif retcode == 0 and retval == 3:
+                if len(inbound[i]) > 0:
+                    computers[i].set_input(inbound[i].popleft())
+                else:
+                    computers[i].set_input(-1)
+
+
+
+    
 
 def read_and_solve():
-	with open('input_23.txt') as f:
-		data = [line.rstrip() for line in f]
-		return solve(data)
+    with open('input_23.txt') as f:
+        data = list(map(int, f.readline().split(',')))
+        return solve(data)
 
 if __name__ == '__main__':
-	print(read_and_solve())
+    print(read_and_solve())
