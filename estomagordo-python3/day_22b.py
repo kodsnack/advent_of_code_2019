c1d3fabb9f82d8bec698aa0e3c6b83659bdbf7da
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2019-12-24 13:23:22 +0100

    thought this was correct :)

--- estomagordo-python3/day_22b.py
+++ estomagordo-python3/day_22b.py
@@ -26,19 +26,20 @@ def run(instructions, size, position):
     for instruction in instructions[::-1]:
         if instruction[-1] == 'stack':
             reversed = not reversed
+            front, back = size - front - 1, size - back - 1
         elif instruction[0] == 'deal':
             increment = int(instruction[-1])
-            if reversed:
-                increment *= -1
+            # if reversed:
+            #     increment *= -1
             front, back = deal_with_increment(front, size, increment), deal_with_increment(back, size, increment)
         else:
             amount = int(instruction[-1])
             if reversed:
-                front, back = (back + amount) % size, (front + amount) % size
+                front, back = (front + amount) % size, (back + amount) % size
             else:
                 front, back = (front + amount) % size, (back + amount) % size
 
-    return back if reversed else front
+    return front
 
 def solve(instructions, size, times, position):
     return run(instructions, size, position)
@@ -47,10 +48,10 @@ def solve(instructions, size, times, position):
 def read_and_solve():
     with open('input_22.txt') as f:
         data = [line.split() for line in f]
-        for x in range(10):
-            print(run(data, 10, x), end=' ')
+        # for x in range(10):
+        #     print(run(data, 10, x), end=' ')
         # return solve(data, 10007, 1, 2019)
-        # return solve(data, 119315717514047 , 101741582076661, 2020)
+        return solve(data, 119315717514047 , 101741582076661, 2020)
 
 if __name__ == '__main__':
     print(read_and_solve())
