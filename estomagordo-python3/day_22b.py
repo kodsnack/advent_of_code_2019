31631ffe6033abed48306cc14ef5d83fbc98841b
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2019-12-24 11:50:43 +0100

    horrible 22b wip

--- estomagordo-python3/day_22b.py
+++ estomagordo-python3/day_22b.py
@@ -4,41 +4,53 @@ from heapq import heappop, heappush
 from collections import Counter, defaultdict
 
 
-def apply(deck, instruction, size):
-    if instruction[-1] == 'stack':
-        return deck[::-1]
+def deal_with_increment(num, size, increment):
+    used = 0
+    pos = 0
 
-    if instruction[0] == 'cut':
-        cut = int(instruction[1])
-        return deck[cut:] + deck[:cut]
-
-    increment = int(instruction[-1])
-
-    val = 0
-    new_deck = [0 for _ in range(size)]
-
-    for x in range(size):
-        new_deck[val] = deck[x]
-        val = (val + increment) % size
-
-    return new_deck
-
-
-def solve(instructions, size):
-    deck = [x for x in range(size)]
-
-    for instruction in instructions:
-        deck = apply(deck, instruction, size)
+    while True:
+        fits = (size - pos - 1) // increment
+        diff = num - pos
         
-    for i, card in enumerate(deck):
-        if card == 2019:
-            return i
+        if diff >= 0 and diff % increment == 0:
+            return used + diff // increment
+
+        pos = (pos + (fits + 1) * increment) % size
+        used += fits + 1
+
+
+def run(instructions, size, position):
+    front, back = position, size - position - 1
+    reversed = False
+
+    for instruction in instructions[::-1]:
+        if instruction[-1] == 'stack':
+            reversed = not reversed
+        elif instruction[0] == 'deal':
+            increment = int(instruction[-1])
+            if reversed:
+                increment *= -1
+            front, back = deal_with_increment(front, size, increment), deal_with_increment(back, size, increment)
+        else:
+            amount = int(instruction[-1])
+            if reversed:
+                front, back = (back + amount) % size, (front + amount) % size
+            else:
+                front, back = (front + amount) % size, (back + amount) % size
+
+    return back if reversed else front
+
+def solve(instructions, size, times, position):
+    return run(instructions, size, position)
     
 
 def read_and_solve():
     with open('input_22.txt') as f:
         data = [line.split() for line in f]
-        return solve(data, 10)
+        for x in range(10):
+            print(run(data, 10, x), end=' ')
+        # return solve(data, 10007, 1, 2019)
+        # return solve(data, 119315717514047 , 101741582076661, 2020)
 
 if __name__ == '__main__':
     print(read_and_solve())
