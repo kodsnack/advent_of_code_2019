42d8fe945ba418a6e197a1a3a0ae25213738e435
Joel Fåk <joel.fak@niradynamics.se>
2019-12-26 00:44:38 +0100

    Remove amplifier code from day 9

--- joelfak-python/day09.py
+++ joelfak-python/day09.py
@@ -2,7 +2,7 @@
 
 from helpfunctions import *
 import unittest, sys
-import operator, itertools
+import operator, itertools, math
 from collections import namedtuple, deque
 
 IntcodeResult = namedtuple('IntcodeResult', ['programDone', 'output'])
@@ -60,11 +60,6 @@ class IncodeComputer:
                 return self.readMemory(self.p+pointerOffset)
             if paramMode == 2: # Relative mode
                 self.printIfVerbose("Relative mode")
-                print("paramMode 2 - relativeBase: {}, offset: {}, pointer: {}, value: {}".format(
-                                    self.relativeBase,
-                                    self.readMemory(self.p+pointerOffset),
-                                    self.relativeBase + self.readMemory(self.p+pointerOffset),
-                                    self.program[self.relativeBase + self.program[self.p+pointerOffset]]))
                 return self.readMemory(self.relativeBase + self.readMemory(self.p+pointerOffset))
         # Position mode
         self.printIfVerbose("Position mode")
@@ -150,9 +145,9 @@ class IncodeComputer:
             elif oc.opcode == 9: # Move relative base
                 self.printInstruction("Move relative base, parameters: {}", 1)
                 parameter = self.getParameter(1, oc.parameterModes)
-                print("move base - current: {}, parameter: {}".format(self.relativeBase, parameter))
+                self.printIfVerbose("move base - current: {}, parameter: {}".format(self.relativeBase, parameter))
                 self.relativeBase += parameter
-                print("new base: {}".format(self.relativeBase))
+                self.printIfVerbose("new base: {}".format(self.relativeBase))
                 self.increaseProgramPointer(2)
 
             else:
@@ -164,60 +159,8 @@ class IncodeComputer:
 
         return IntcodeResult(programDone, output)
 
-def runAmplifiers(program, phaseSettings, verbose=False):
-    printIfVerbose("runAmplifiers with phase settings: {}".format(phaseSettings), verbose)
-    ampA = IncodeComputer(program, "ampA", verbose)
-    ampB = IncodeComputer(program, "ampB", verbose)
-    ampC = IncodeComputer(program, "ampC", verbose)
-    ampD = IncodeComputer(program, "ampD", verbose)
-    ampE = IncodeComputer(program, "ampE", verbose)
-
-    programDone = 0
-    signal = 0
-
-    inputA = [phaseSettings[0]]
-    inputB = [phaseSettings[1]]
-    inputC = [phaseSettings[2]]
-    inputD = [phaseSettings[3]]
-    inputE = [phaseSettings[4]]
-    inputA.append(0)
-
-    while programDone == False:
-        printIfVerbose("inputA: {}".format(inputA), verbose)
-        res = ampA.runUntilHalt(inputA)
-        if len(res.output) > 0:
-            inputB.append(res.output[0])
-
-        printIfVerbose("inputB: {}".format(inputB), verbose)
-        res = ampB.runUntilHalt(inputB)
-        if len(res.output) > 0:
-            inputC.append(res.output[0])
-
-        printIfVerbose("inputC: {}".format(inputC), verbose)
-        res = ampC.runUntilHalt(inputC)
-        if len(res.output) > 0:
-            inputD.append(res.output[0])
-
-        printIfVerbose("inputD: {}".format(inputD), verbose)
-        res = ampD.runUntilHalt(inputD)
-        if len(res.output) > 0:
-            inputE.append(res.output[0])
-
-        printIfVerbose("inputE: {}".format(inputE), verbose)
-        res = ampE.runUntilHalt(inputE)
-        if len(res.output) > 0:
-            inputA.append(res.output[0])
-
-        programDone = res.programDone
-
-    return res.output[0]
-
-def findBestAmplifierPhaseSettings(program, startingParameters):
-    bestParams = max(itertools.permutations(startingParameters), key=lambda x: runAmplifiers(program, x))
-    return (list(bestParams), runAmplifiers(program, bestParams))
-
 def part1(program):
-    return findBestAmplifierPhaseSettings(program, range(0,5))[1]
+    return IncodeComputer(program).runUntilHalt([1]).output
 
 def part2(program):
     return findBestAmplifierPhaseSettings(program, range(5,10))[1]
@@ -313,75 +256,22 @@ class TestDay05_part2(unittest.TestCase):
     def test_intcode_if_input_is_above_8(self):
         self.assertEqual(IncodeComputer([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]).runUntilHalt([9]).output[0], 1001)
 
-
-class TestDay07_part1(unittest.TestCase):
-    def test_example_program_1(self):
-        self.assertEqual(runAmplifiers([3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0],
-                                        [4,3,2,1,0]),
-                                        43210)
-
-    def test_example_program_2(self):
-        self.assertEqual(runAmplifiers([3,23,3,24,1002,24,10,24,1002,23,-1,23,
-                                        101,5,23,23,1,24,23,23,4,23,99,0,0],
-                                        [0,1,2,3,4]),
-                                        54321)
-
-    def test_example_program_3(self):
-        self.assertEqual(runAmplifiers([3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,
-                                        1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0],
-                                        [1,0,4,3,2]),
-                                        65210)
-
-    def test_findBestAmplifierPhaseSettings_example_program_1(self):
-        self.assertEqual(findBestAmplifierPhaseSettings([3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0],
-                                                        [0,1,2,3,4]),
-                                                        ([4,3,2,1,0], 43210))
-
-    def test_findBestAmplifierPhaseSettings_example_program_2(self):
-        self.assertEqual(findBestAmplifierPhaseSettings([3,23,3,24,1002,24,10,24,1002,23,-1,23,
-                                                        101,5,23,23,1,24,23,23,4,23,99,0,0],
-                                                        [0,1,2,3,4]),
-                                                        ([0,1,2,3,4], 54321))
-
-    def test_findBestAmplifierPhaseSettings_example_program_3(self):
-        self.assertEqual(findBestAmplifierPhaseSettings([3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,
-                                                        1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0],
-                                                        [0,1,2,3,4]),
-                                                        ([1,0,4,3,2], 65210))
-
-class TestDay07_part2(unittest.TestCase):
-    def test_part2_example_program_1(self):
-        self.assertEqual(runAmplifiers([3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
-                                        27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5],
-                                        [9,8,7,6,5]),
-                                        139629729)
-
-    def test_part2_example_program_2(self):
-        self.assertEqual(runAmplifiers([3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
-                                        -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
-                                        53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10],
-                                        [9,7,8,5,6]),
-                                        18216)
-
-    def test_part2_findBestAmplifierPhaseSettings_example_program_1(self):
-        self.assertEqual(findBestAmplifierPhaseSettings([3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
-                                                        27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5],
-                                                        [5,6,7,8,9]),
-                                                        ([9,8,7,6,5], 139629729))
-
-    def test_part2_findBestAmplifierPhaseSettings_example_program_1(self):
-        self.assertEqual(findBestAmplifierPhaseSettings([3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
-                                                        -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
-                                                        53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10],
-                                                        [5,6,7,8,9])[1],    # finds solution    [9, 7, 8, 5, 6]
-                                                        18216)              # which is equal to [9, 8, 7, 6, 5]
-
 class TestDay09_part1(unittest.TestCase):
     def test_self_copy_program(self):
-        print()
-        ic = IncodeComputer([109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99], verbose=True)
-        ic.runUntilHalt()
-        self.assertEqual(ic.getUsedProgram(), [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99])
+        ic = IncodeComputer([109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99])
+        self.assertEqual(ic.runUntilHalt().output, [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99])
+
+    def test_output_16_digit_number(self):
+        ic = IncodeComputer([1102,34915192,34915192,7,4,7,99,0])
+        res = ic.runUntilHalt()
+        print("output: {}".format(res.output))
+        numDigits = int(math.log10(res.output[0]))+1
+        self.assertEqual(numDigits, 16)
+
+    def test_large_number(self):
+        ic = IncodeComputer([104,1125899906842624,99])
+        res = ic.runUntilHalt()
+        self.assertEqual(res.output[0], 1125899906842624)
 
 ## Main ########################################################
 
