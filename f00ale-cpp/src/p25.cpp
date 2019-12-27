96186541d32898ba195b2d0fc1ba222c462e0ca0
Arno Lepisk <arno@lepisk.se>
2019-12-27 09:48:45 +0100

    Day 25 cleaned up.

--- f00ale-cpp/src/p25.cpp
+++ f00ale-cpp/src/p25.cpp
@@ -3,78 +3,39 @@
 #include <tuple>
 #include <set>
 #include <queue>
-#include <map>
-#include <unordered_set>
 
 #include "intcode.h"
 
 void p25(std::istream & is) {
     int ans1 = 0;
-    int ans2 = 0;
+    std::string ans2 = "-";
     const auto program = readIntcode(is);
 
-    std::queue<std::tuple<intcodemachine, std::set<std::string>,std::string>> q;
+    std::queue<std::tuple<intcodemachine, std::set<std::string>>> q;
     std::set<std::tuple<std::string, std::set<std::string>>> visited;
 
-    auto splitstring = [](const std::string & s) {
-        std::vector<std::string> ret;
-        std::string::size_type pos = 0;
-        auto n = s.find('\n');
-        while(n != std::string::npos) {
-            if(n-pos > 0)
-            ret.push_back(s.substr(pos, n-pos));
-            pos = n + 1;
-
-            n = s.find('\n', pos);
-        }
-        return ret;
-    };
-
-    const std::unordered_set<std::string> forbidden {
-        "infinite loop",
-        "molten lava",
-        "escape pod",
-        "giant electromagnet",
-        "photons"
-    };
-
-    q.emplace(intcodemachine(program), std::set<std::string>(), "");
-    while(!q.empty()) {
-        auto [mach, inventory, last] = q.front();
+    q.emplace(intcodemachine(program), std::set<std::string>());
+    while(!q.empty() && !ans1) {
+        auto [mach, inventory] = q.front();
         q.pop();
         bool done = false;
-        std::string output;
+        std::vector<std::string> output;
+        std::string currentoutput;
         while (!done) {
             while (mach.step());
             switch (mach.state) {
                 case intcodemachine::RUNNING:
                     // do nothing, we should not end up here
                     break;
-                case intcodemachine::WAITING:
-                {
-                    //std::cout << output << std::endl;
-                    auto strs = splitstring(output);
-                    for(auto & s : strs){
-                        if(s == "Unrecognized command.") {
-                            std::cout << s << std::endl;
-                            std::cout << last << std::endl;
-                            exit(0);
-                        }
-                    }
-                    if(strs.size() == 0) {
-                        done = true;
-                        break;
-                    }
+                case intcodemachine::WAITING: {
                     std::string room;
-                    for(auto & s : strs) {
+                    for(const auto & s : output) {
                         if(s[0] == '=') {
                             room = s;
                         }
                     }
                     if(room.empty()) {
-                        std::cout << "xxx" << output << std::endl;
-                        std::cout << "yyy" << last << std::endl;
-                        exit(0);
+                        done = true;
                         break;
                     }
                     if(visited.count(std::tuple(room, inventory))) {
@@ -83,17 +44,10 @@ void p25(std::istream & is) {
                     }
                     visited.emplace(room, inventory);
 
-
-                    std::cout << room;
-                    for(auto & i : inventory) std::cout << " " << i;
-                    std::cout << std::endl;
-
                     std::vector<std::string> stuff, dirs;
-                    for(const auto & s : strs) {
+                    for(const auto & s : output) {
                         if(s[0] == '=' && s != room) {
-                            std::cout << "two rooms!" << std::endl;
-                            stuff.clear();
-                            dirs.clear();
+                            // two rooms in output
                             exit(0);
                         }
                         if(s[0] == '-') {
@@ -103,54 +57,48 @@ void p25(std::istream & is) {
                         }
                     }
 
-                    for(auto & dir : dirs) {
+                    for(const auto & dir : dirs) {
                         {
                             auto cpy = mach;
-                            auto cmd = dir + "\n";
+                            auto cmd = dir + '\n';
                             for (auto c : cmd) cpy.addInput(c);
-                            q.emplace(cpy, inventory, output + dir);
+                            q.emplace(cpy, inventory);
                         }
-                        for(auto & st : stuff) {
-                            if(inventory.find(st) != inventory.end()) continue;
-                            if(forbidden.find(st) != forbidden.end()) continue;
-                            std::string cmd = "take " + st + "\n" + dir + "\n";
+                        for(const auto & st : stuff) {
+                            std::string cmd = "take " + st + '\n' + dir + '\n';
                             auto cpy = mach;
                             for(auto c : cmd) cpy.addInput(c);
                             auto icpy = inventory;
                             icpy.emplace(st);
-                            q.emplace(cpy, icpy, output + cmd);
+                            q.emplace(cpy, icpy);
                         }
                     }
-
-
-                    //
-//                    auto cpy = mach;
-//                    for(auto c : arg) {
-//                        cpy.addInput(c);
-//                    }
-//                    cpy.addInput(10);
-//                    q.emplace(cpy, inventory);
-
-
-                }   done = true;
+                }
+                    done = true;
                     break;
                 case intcodemachine::OUTPUT:
-                    output.push_back(mach.output);
-                    if(output.size() > 5000) {
-                        std::cout << "long output" << std::endl;
-                        std::cout << last << std::endl;
-                        std::cout << ":::::" << std::endl;
-                        std::cout << output << std::endl;
-                        exit(0);
+                    if(mach.output >= '0' && mach.output <= '9') {
+                        ans1 = 10 * ans1 + (mach.output - '0');
+                    } else if(ans1 > 10) {
+                        done = true;
+                        break;
+                    } else {
+                        ans1 = 0;
                     }
+                    if(mach.output == 10) {
+                        // this takes care of infinite loop
+                        if(!output.empty() && output.back() == currentoutput) done = true;
+
+                        if(!currentoutput.empty()) {
+                            output.emplace_back(std::move(currentoutput));
+                            currentoutput.clear();
+                        }
+                    } else {
+                        currentoutput.push_back(mach.output);
+                    }
+
                     break;
                 case intcodemachine::TERMINATED:
-
-                    std::cout <<"terminated" << std::endl;
-                    std::cout << last << std::endl;
-                    std::cout <<"<>>" << std::endl;
-                    std::cout << output << std::endl;
-                    std::cout <<"----" << std::endl;
                     done = true;
                     break;
             }
@@ -161,9 +109,6 @@ void p25(std::istream & is) {
     std::cout << ans2 << std::endl;
 }
 
-#include <fstream>
 int main() {
-    //p25(std::cin);exit(0);
-    std::ifstream is("/Users/arno/git/aoc19/f00ale-cpp/data/p25.txt");
-    p25(is);
+    p25(std::cin);
 }
