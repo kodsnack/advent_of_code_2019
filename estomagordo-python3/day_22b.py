2848e6ef94f52ffa847644fd56d5ca9aed62023b
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2020-10-18 21:28:54 +0200

    apparently I was in the middle of something waybackwhen

--- estomagordo-python3/day_22b.py
+++ estomagordo-python3/day_22b.py
@@ -4,6 +4,23 @@ from heapq import heappop, heappush
 from collections import Counter, defaultdict
 
 
+def extended_euclid(a, b):
+    if a == 0 :   
+        return b, 0, 1
+             
+    gcd, x1, y1 = extended_euclid(b % a, a) 
+    
+    x = y1 - (b // a) * x1  
+    y = x1  
+     
+    return gcd, x, y
+
+
+def modsolver(a, b, n):
+    _, xp, _ = extended_euclid(a, b)
+    return (xp * b) % n
+
+
 def deal_with_increment(num, size, increment):
     used = 0
     pos = 0
@@ -20,13 +37,13 @@ def deal_with_increment(num, size, increment):
 
 
 def run(instructions, size, position):
-    orig = position
     for instruction in instructions[::-1]:
         if instruction[-1] == 'stack':
             position = size - position - 1
         elif instruction[0] == 'deal':
             increment = int(instruction[-1])
-            deal_with_increment(position, size, increment)#position = increment * orig
+            position = modsolver(increment, position, size)
+            # deal_with_increment(position, size, increment)#position = increment * orig
         else:
             amount = int(instruction[-1])
             position = (position + amount) % size
@@ -41,8 +58,13 @@ def solve(instructions, size, times, position):
 def read_and_solve():
     with open('input_22.txt') as f:
         data = [line.split() for line in f]
-        print(solve(data, 10007, 1, 2019))
-        return solve(data, 119315717514047 , 101741582076661, 2020)
+        # print(solve(data, 10, 1, 3))
+        # print(solve(data, 10007, 1, 2019))
+        print(solve(data, 10, 3, 0))
+        print(solve(data, 10007, 1, 6326))
+        # return solve(data, 119315717514047 , 101741582076661, 2020)
 
 if __name__ == '__main__':
     print(read_and_solve())
+
+print(modsolver(3, 6, 10))
