d3034a58f24c7c5205ac2a25159fd3f79d14e26a
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2020-10-27 19:43:05 +0100

    ugly wip

--- estomagordo-python3/day_22b.py
+++ estomagordo-python3/day_22b.py
@@ -36,13 +36,23 @@ def deal_with_increment(num, size, increment):
         used += fits + 1
 
 
+def shuffler(increment, position, size):
+    overshoot = 0
+
+    while (size * overshoot + position) % increment:
+        overshoot += 1
+
+    return (size * overshoot + position) // increment
+
+
 def run(instructions, size, position):
     for instruction in instructions[::-1]:
         if instruction[-1] == 'stack':
             position = size - position - 1
         elif instruction[0] == 'deal':
             increment = int(instruction[-1])
-            position = modsolver(increment, position, size)
+            # position = modsolver(increment, position, size)
+            position = shuffler(increment, position, size)
             # deal_with_increment(position, size, increment)#position = increment * orig
         else:
             amount = int(instruction[-1])
@@ -52,7 +62,12 @@ def run(instructions, size, position):
 
 
 def solve(instructions, size, times, position):
-    return run(instructions, size, position)
+    from collections import Counter
+    c = Counter()
+    for _ in range(times):
+        position = run(instructions, size, position)
+        c[position] += 1
+    return [val for val in c if c[val] > 1]
     
 
 def read_and_solve():
@@ -60,11 +75,16 @@ def read_and_solve():
         data = [line.split() for line in f]
         # print(solve(data, 10, 1, 3))
         # print(solve(data, 10007, 1, 2019))
-        print(solve(data, 10, 3, 0))
-        print(solve(data, 10007, 1, 6326))
-        # return solve(data, 119315717514047 , 101741582076661, 2020)
+        # print(solve(data, 10007, 1, 6326))
+        # from random import shuffle
+        # l = list(data)
+        # shuffle(l)
+        # print(solve(l, 119315717514047, 1, 2020))
+        # print(119315717514047)
+        return solve(data, 119315717514047, 30000, 2020)
+        # return solve(data, 119315717514047, 101741582076661, 2020)
 
 if __name__ == '__main__':
     print(read_and_solve())
 
-print(modsolver(3, 6, 10))
+# print(modsolver(3, 6, 10))
